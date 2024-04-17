83f31728cef651c64b5062587e943b2193fe3627
Santiago Cotto <scotto@goshops.ai>
2024-04-17 16:48:09 -0300

    asd

--- Controller/StoreSessionData.php
+++ Controller/StoreSessionData.php
@@ -3,27 +3,32 @@ namespace Gopersonal\Magento\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
 use Gopersonal\Magento\Model\SessionDataRepository;
-
 
 class StoreSessionData extends Action
 {
     protected $sessionDataRepository;
+    protected $resultJsonFactory;
 
     public function __construct(
         Context $context,
-        SessionDataRepository $sessionDataRepository
+        SessionDataRepository $sessionDataRepository,
+        JsonFactory $resultJsonFactory
     ) {
         parent::__construct($context);
         $this->sessionDataRepository = $sessionDataRepository;
+        $this->resultJsonFactory = $resultJsonFactory;
     }
-    
+
     public function execute()
     {
         $data = $this->getRequest()->getParam('data');
         $this->sessionDataRepository->storeSessionData($data);
-        $response = $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_JSON);
-        $response->setData(['message' => 'Session data stored successfully']);
-        return $response;
+
+        $resultJson = $this->resultJsonFactory->create();
+        $resultJson->setData(['message' => 'Session data stored successfully']);
+
+        return $resultJson;
     }
 }
