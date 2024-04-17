b2664dc2ef0dd44e97a6d9118fe020be4ebc9295
Santiago Cotto <scotto@goshops.ai>
2024-04-17 15:51:53 -0300

    fixes

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Mycompany_ConsoleLog',
+    'Gopersonal_Magento',
     __DIR__
 );
