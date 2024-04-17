c1e08882e474d3c8a2c595b39dcf3b8305c34e97
Santiago Cotto <scotto@goshops.ai>
2024-04-17 17:48:33 -0300

    sdsd

--- Observer/AddToCart.php
+++ Observer/AddToCart.php
@@ -24,35 +24,36 @@ class AddToCart implements ObserverInterface
 
     public function execute(Observer $observer)
     {
-        $item = $observer->getEvent()->getData('quote_item');
-        $product = $item->getProduct();
-        $productId = $product->getId();
-        $productSku = $product->getSku();
-
-        $token = $this->customerSession->getData('api_token');
-        
-        if (!$token) {
-            $this->logger->info('No API token found in session.');
-            return;
-        }
-
-        $this->curl->addHeader("Authorization", "Bearer " . $token);
-        $this->curl->addHeader("Content-Type", "application/json");
-        
-        $postData = json_encode([
-            'productId' => $productId,
-            'productSku' => $productSku
-        ]);
-
         try {
+            $item = $observer->getEvent()->getData('quote_item');
+            $product = $item ? $item->getProduct() : null;
+            $productId = $product ? $product->getId() : 'Product ID not found';
+            $productSku = $product ? $product->getSku() : 'SKU not found';
+
+            $token = $this->customerSession->getData('api_token');
+            
+            if (!$token) {
+                $this->logger->info('No API token found in session.');
+                return;
+            }
+
+            $this->curl->addHeader("Authorization", "Bearer " . $token);
+            $this->curl->addHeader("Content-Type", "application/json");
+            
+            $postData = json_encode([
+                'productId' => $productId,
+                'productSku' => $productSku
+            ]);
+
             $this->curl->post('https://worker-small-frog-11cb.go-shops.workers.dev/', $postData);
-            if ($this->curl->getStatus() == 200) {
-                $this->logger->info('AddToCart event: Data posted successfully.', ['response' => $this->curl->getBody()]);
-            } else {
+            if ($this->curl->getStatus() != 200) {
                 $this->logger->error('AddToCart event: API call failed.', ['status' => $this->curl->getStatus(), 'response' => $this->curl->getBody()]);
+                throw new LocalizedException(__('Failed to handle AddToCart event.'));
             }
         } catch (\Exception $e) {
             $this->logger->error('AddToCart event: Exception occurred.', ['exception' => $e->getMessage()]);
+            throw new LocalizedException(__('Error adding product to cart.'));
         }
     }
+
 }
