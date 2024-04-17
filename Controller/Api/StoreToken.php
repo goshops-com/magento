29ea8fb41ab48afc12fca273f16d98ee5be62a5d
Santiago Cotto <scotto@goshops.ai>
2024-04-17 16:56:08 -0300

    fixes

---
+++ Controller/Api/StoreToken.php
@@ -0,0 +1,30 @@
+<?php
+namespace GoPersonal\Magento\Controller\Api;
+
+class StoreToken extends \Magento\Framework\App\Action\Action
+{
+    protected $customerSession;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Customer\Model\Session $customerSession
+    ) {
+        $this->customerSession = $customerSession;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $token = $this->getRequest()->getParam('token');
+        if ($token) {
+            $this->customerSession->setData('api_token', $token);
+            $result = __('Token stored successfully.');
+        } else {
+            $result = __('No token provided.');
+        }
+
+        $response = $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_RAW);
+        $response->setContents($result);
+        return $response;
+    }
+}
