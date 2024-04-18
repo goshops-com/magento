b0b9884074d08869dd10e43c29f35bd22a62d403
Santiago Cotto <scotto@goshops.ai>
2024-04-17 22:46:30 -0300

    fixes

--- Block/ConsoleLog.php
+++ Block/ConsoleLog.php
@@ -3,25 +3,50 @@ namespace Gopersonal\Magento\Block;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
-use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\Registry;
 
 class ConsoleLog extends Template
 {
-    protected $_scopeConfig;
+    protected $_request;
+    protected $_registry;
 
     public function __construct(
         Context $context,
+        Http $request,
+        Registry $registry,
         array $data = []
     ) {
         parent::__construct($context, $data);
-        $this->_scopeConfig = $context->getScopeConfig();
+        $this->_request = $request;
+        $this->_registry = $registry;
     }
 
-    public function getClientId()
+    public function isHomePage()
     {
-        return $this->_scopeConfig->getValue(
-            'gopersonal/general/client_id',
-            ScopeInterface::SCOPE_STORE
-        );
+        return $this->_request->getFullActionName() == 'cms_index_index';
+    }
+
+    public function isProductPage()
+    {
+        return $this->_request->getFullActionName() == 'catalog_product_view';
+    }
+
+    public function isCartPage()
+    {
+        return $this->_request->getFullActionName() == 'checkout_cart_index';
+    }
+
+    public function isCheckoutPage()
+    {
+        return $this->_request->getFullActionName() == 'checkout_index_index';
+    }
+
+    public function getCurrentProductId()
+    {
+        if ($this->isProductPage()) {
+            return $this->_registry->registry('current_product') ? $this->_registry->registry('current_product')->getId() : null;
+        }
+        return null;
     }
 }
