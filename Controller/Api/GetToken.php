2d37ac828eccde7cbac9f6a968e906423e7407c5
Santiago Cotto <scotto@goshops.ai>
2024-04-23 09:46:50 -0300

    fixes

--- Controller/Api/GetToken.php
+++ Controller/Api/GetToken.php
@@ -1,29 +1,47 @@
 <?php
 namespace GoPersonal\Magento\Controller\Api;
 
-class GetToken extends \Magento\Framework\App\Action\Action
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\Controller\Result\JsonFactory;
+
+class GetToken extends Action
 {
     protected $customerSession;
+    protected $resultJsonFactory;
 
     public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Customer\Model\Session $customerSession
+        Context $context,
+        CustomerSession $customerSession,
+        JsonFactory $resultJsonFactory
     ) {
-        $this->customerSession = $customerSession;
         parent::__construct($context);
+        $this->customerSession = $customerSession;
+        $this->resultJsonFactory = $resultJsonFactory;
     }
 
     public function execute()
     {
+        $result = $this->resultJsonFactory->create();
+
+        // Retrieve the JWT token from the session
         $token = $this->customerSession->getData('gopersonal_jwt');
-        if ($token) {
-            $result = __('Stored Token: %1', $token);
+
+        // Prepare data to return
+        $data = [
+            'token' => $token ? $token : 'No token is stored.'
+        ];
+
+        // Check if the customer is logged in
+        if ($this->customerSession->isLoggedIn()) {
+            $data['customer_id'] = $this->customerSession->getCustomer()->getId();
+            $data['customer_email'] = $this->customerSession->getCustomer()->getEmail();
         } else {
-            $result = __('No token is stored.');
+            $data['customer_id'] = null;
+            $data['customer_email'] = null;
         }
 
-        $response = $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_RAW);
-        $response->setContents($result);
-        return $response;
+        return $result->setData($data);
     }
 }
