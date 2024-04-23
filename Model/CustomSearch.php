8f21e3c494af2c721cf28c022d6c41f09a7fb09e
Santiago Cotto <scotto@goshops.ai>
2024-04-23 15:55:25 -0300

    asd

--- Model/CustomSearch.php
+++ Model/CustomSearch.php
@@ -3,27 +3,31 @@ namespace Gopersonal\Magento\Model;
 
 use Magento\Search\Api\SearchInterface;
 use Magento\Framework\Api\Search\SearchCriteriaInterface;
+use Magento\Framework\Api\Search\SearchResultFactory;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
-use Magento\Search\Model\SearchEngine as DefaultSearchEngine;
+use Magento\Search\Api\SearchInterface as DefaultSearchInterface;
 
 class CustomSearch implements SearchInterface {
     protected $httpClient;
     protected $scopeConfig;
     protected $resultJsonFactory;
-    protected $defaultSearchEngine;
+    protected $defaultSearchEngine; // Ensure this variable is used appropriately
+    protected $searchResultFactory;
 
     public function __construct(
         \Magento\Framework\HTTP\ClientInterface $httpClient,
         ScopeConfigInterface $scopeConfig,
         JsonFactory $resultJsonFactory,
-        DefaultSearchEngine $defaultSearchEngine // Injecting the default search engine
+        DefaultSearchInterface $defaultSearchEngine, // This should implement SearchInterface
+        SearchResultFactory $searchResultFactory
     ) {
         $this->httpClient = $httpClient;
         $this->scopeConfig = $scopeConfig;
         $this->resultJsonFactory = $resultJsonFactory;
-        $this->defaultSearchEngine = $defaultSearchEngine; // Store the default search engine
+        $this->defaultSearchEngine = $defaultSearchEngine;
+        $this->searchResultFactory = $searchResultFactory;
     }
 
     public function search(SearchCriteriaInterface $searchCriteria) {
@@ -33,12 +37,20 @@ class CustomSearch implements SearchInterface {
         );
 
         if ($isEnabled == 'YES') {
+            // Create a JSON response with hardcoded product data
             $result = $this->resultJsonFactory->create();
             $productData = [
-                'product_id' => 45,
-                'name' => 'Sample Product',
-                'price' => 99.99,
-                'description' => 'This is a sample product from hardcoded search result.'
+                'items' => [
+                    [
+                        'product_id' => 45,
+                        'name' => 'Sample Product',
+                        'price' => 99.99,
+                        'description' => 'This is a sample product from hardcoded search result.'
+                    ]
+                ],
+                'total_count' => 1,
+                'search_criteria' => $searchCriteria,
+                'aggregations' => null
             ];
             return $result->setData($productData);
         } else {
@@ -47,4 +59,3 @@ class CustomSearch implements SearchInterface {
         }
     }
 }
-
