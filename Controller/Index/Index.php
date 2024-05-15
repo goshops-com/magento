61e95c2db939e9e944a77f9862afb2b6b99c2b85
Santiago Cotto <scotto@goshops.ai>
2024-05-14 21:23:56 -0300

    asd

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,40 +1,47 @@
 <?php
-namespace GoPersonal\Magento\Controller\Index; // Adjusted namespace
+namespace GoPersonal\Magento\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\RawFactory;
 use Magento\Framework\View\Asset\Repository;
+use Magento\Framework\App\RequestInterface;
 
 class Index extends Action
 {
     protected $resultRawFactory;
     protected $assetRepository;
-    protected $directoryList; // Add this line
+    protected $directoryList;
 
     public function __construct(
         Context $context,
         RawFactory $resultRawFactory,
         Repository $assetRepository,
-        \Magento\Framework\App\Filesystem\DirectoryList $directoryList // Add this line
+        \Magento\Framework\App\Filesystem\DirectoryList $directoryList
     ) {
         parent::__construct($context);
         $this->resultRawFactory = $resultRawFactory;
         $this->assetRepository = $assetRepository;
-        $this->directoryList = $directoryList; // Add this line
+        $this->directoryList = $directoryList;
     }
 
     public function execute()
     {
-        $resultRaw = $this->resultRawFactory->create();
+        $request = $this->getRequest();
+        $requestedPath = $request->getPathInfo();
 
-        $moduleDir = $this->directoryList->getPath('app');
-        $filePath = $moduleDir . '/code/Gopersonal/Magento/web/gp-firebase.js';
+        if ($requestedPath === '/gp-firebase' || $requestedPath === '/gp-firebase.js') {
+            $resultRaw = $this->resultRawFactory->create();
+            $moduleDir = $this->directoryList->getPath('app');
+            $filePath = $moduleDir . '/code/Gopersonal/Magento/web/gp-firebase.js';
+            $jsContent = file_get_contents($filePath);
+            $resultRaw->setContents($jsContent);
+            $resultRaw->setHeader('Content-Type', 'text/javascript');
+            return $resultRaw;
+        }
 
-        $jsContent = file_get_contents($filePath);
-
-        $resultRaw->setContents($jsContent);
-        $resultRaw->setHeader('Content-Type', 'text/javascript'); 
-        return $resultRaw;
+        // Handle other paths or return a 404 error
+        $this->getResponse()->setHttpResponseCode(404);
+        return $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_RAW)->setContents('File not found');
     }
 }
