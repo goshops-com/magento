56979abe4ffd437f48b4d298195bd328c70b5882
Santiago Cotto <scotto@goshops.ai>
2024-05-21 21:51:54 -0300

    fixes

---
+++ Block/Search/Result.php
@@ -0,0 +1,20 @@
+<?php
+namespace Gopersonal\Magento\Block\Search;
+
+use Magento\Framework\View\Element\Template;
+
+class Result extends \Magento\Framework\View\Element\Template
+{
+    protected function _construct()
+    {
+        parent::_construct();
+        // Disable cache for this block
+        $this->setCacheLifetime(0);
+    }
+
+    public function getCacheKeyInfo()
+    {
+        // Return false to completely disable caching
+        return false;
+    }
+}
