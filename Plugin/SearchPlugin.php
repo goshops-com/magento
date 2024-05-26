4eac39d887ed4d5219a0e294b266cc2ef3fe2319
Santiago Cotto <scotto@goshops.ai>
2024-05-26 19:51:39 -0300

    asd

--- Plugin/SearchPlugin.php
+++ Plugin/SearchPlugin.php
@@ -3,17 +3,23 @@ namespace Gopersonal\Magento\Plugin;
 
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection;
 use Magento\Framework\App\RequestInterface;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
 use Magento\Store\Model\StoreManagerInterface;
 
 class SearchPlugin
 {
     protected $request;
     protected $storeManager;
+    protected $productCollectionFactory;
 
-    public function __construct(RequestInterface $request, StoreManagerInterface $storeManager)
-    {
+    public function __construct(
+        RequestInterface $request,
+        StoreManagerInterface $storeManager,
+        ProductCollectionFactory $productCollectionFactory
+    ) {
         $this->request = $request;
         $this->storeManager = $storeManager;
+        $this->productCollectionFactory = $productCollectionFactory;
     }
 
     public function aroundLoad(Collection $subject, \Closure $proceed, $printQuery = false, $logQuery = false)
@@ -21,14 +27,12 @@ class SearchPlugin
         // Check if the current request is a search request
         if ($this->isSearchRequest()) {
             // Override the search result
-            $subject->getSelect()->reset(\Magento\Framework\DB\Select::FROM);
-            $subject->getSelect()->reset(\Magento\Framework\DB\Select::COLUMNS);
-            $subject->getSelect()->reset(\Magento\Framework\DB\Select::WHERE);
-            $subject->getSelect()->from(
-                ['e' => $subject->getTable('catalog_product_entity')],
-                ['entity_id']
-            );
-            $subject->getSelect()->where('e.entity_id = ?', 1556);
+            $productCollection = $this->productCollectionFactory->create();
+            $productCollection->addFieldToFilter('entity_id', 1556);
+            $subject->clear();
+            foreach ($productCollection as $product) {
+                $subject->addItem($product);
+            }
 
             return $subject;
         }
