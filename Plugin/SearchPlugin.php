de58fa64cca6f439f08b481e675cfd1eb374f262
Santiago Cotto <scotto@goshops.ai>
2024-05-26 18:58:57 -0300

    asd

--- Plugin/SearchPlugin.php
+++ Plugin/SearchPlugin.php
@@ -2,13 +2,25 @@
 namespace Gopersonal\Magento\Plugin;
 
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection;
+use Psr\Log\LoggerInterface;
 
 class SearchPlugin
 {
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function beforeAddSearchFilter(Collection $subject, $query)
     {
+        $this->logger->info('SearchPlugin beforeAddSearchFilter called with query: ' . $query);
+
         // Hardcode the product ID for testing purposes
         $hardcodedProductIds = [1556];
+
+        $this->logger->info('Filtering search results to include only product ID: ' . implode(', ', $hardcodedProductIds));
 
         $subject->addFieldToFilter('entity_id', ['in' => $hardcodedProductIds]);
 
