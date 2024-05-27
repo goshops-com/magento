716e9f9f7fc73490ede875c6d69ae0126d3cf495
Santiago Cotto <scotto@goshops.ai>
2024-05-27 14:11:46 -0300

    asd

--- Plugin/SearchOverride.php
+++ Plugin/SearchOverride.php
@@ -4,14 +4,17 @@ namespace Gopersonal\Magento\Plugin;
 
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection as SearchCollection;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\App\Request\Http;
 
 class SearchOverride
 {
     protected $resourceConnection;
+    protected $request;
 
-    public function __construct(ResourceConnection $resourceConnection)
+    public function __construct(ResourceConnection $resourceConnection, Http $request)
     {
         $this->resourceConnection = $resourceConnection;
+        $this->request = $request;
     }
 
     public function aroundLoad(
@@ -20,11 +23,15 @@ class SearchOverride
         $printQuery = false,
         $logQuery = false
     ) {
-        $fixedProductIds = [1556]; // Your array of fixed product IDs (or fetch dynamically)
+        $searchQuery = $this->request->getParam('q'); // Get the search query parameter
 
-        if (!empty($fixedProductIds)) {
-            $subject->getSelect()->reset(\Zend_Db_Select::WHERE);
-            $subject->getSelect()->where('e.entity_id IN (?)', $fixedProductIds);
+        if ($searchQuery) {
+            $fixedProductIds = [1556]; // Your array of fixed product IDs (or fetch dynamically)
+
+            if (!empty($fixedProductIds)) {
+                $subject->getSelect()->reset(\Zend_Db_Select::WHERE);
+                $subject->getSelect()->where('e.entity_id IN (?)', $fixedProductIds);
+            }
         }
 
         return $proceed($printQuery, $logQuery); // Let the original load method proceed with the modified query
