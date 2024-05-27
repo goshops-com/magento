79eab6e74bdbf7db7057e1943dbf9021d32d8dd0
Santiago Cotto <scotto@goshops.ai>
2024-05-27 14:39:16 -0300

    fixes

--- Plugin/SearchOverride.php
+++ Plugin/SearchOverride.php
@@ -7,21 +7,25 @@ use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\App\Request\Http;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\Stdlib\CookieManagerInterface;
 
 class SearchOverride
 {
     protected $resourceConnection;
     protected $request;
     protected $scopeConfig;
+    protected $cookieManager;
 
     public function __construct(
         ResourceConnection $resourceConnection,
         Http $request,
-        ScopeConfigInterface $scopeConfig
+        ScopeConfigInterface $scopeConfig,
+        CookieManagerInterface $cookieManager
     ) {
         $this->resourceConnection = $resourceConnection;
         $this->request = $request;
         $this->scopeConfig = $scopeConfig;
+        $this->cookieManager = $cookieManager;
     }
 
     public function aroundLoad(
@@ -35,9 +39,10 @@ class SearchOverride
             'gopersonal/general/gopersonal_has_search',
             ScopeInterface::SCOPE_STORE
         );
+        $token = $this->cookieManager->getCookie('gopersonal_jwt'); // Get the token from the cookie
 
-        if ($searchQuery && $isEnabled === 'YES') {
-            $fixedProductIds = [1556]; // Your array of fixed product IDs (or fetch dynamically)
+        if ($searchQuery && $isEnabled === 'YES' && $token !== null) {
+            $fixedProductIds = [123, 456, 789]; // Your array of fixed product IDs (or fetch dynamically)
 
             if (!empty($fixedProductIds)) {
                 $subject->getSelect()->reset(\Zend_Db_Select::WHERE);
