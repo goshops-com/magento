cfcd16216ee4cbf85646e6050df2607419a54853
Santiago Cotto <scotto@goshops.ai>
2024-05-27 14:18:13 -0300

    aasd

--- Plugin/SearchOverride.php
+++ Plugin/SearchOverride.php
@@ -5,16 +5,23 @@ namespace Gopersonal\Magento\Plugin;
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection as SearchCollection;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\App\Request\Http;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class SearchOverride
 {
     protected $resourceConnection;
     protected $request;
+    protected $scopeConfig;
 
-    public function __construct(ResourceConnection $resourceConnection, Http $request)
-    {
+    public function __construct(
+        ResourceConnection $resourceConnection,
+        Http $request,
+        ScopeConfigInterface $scopeConfig
+    ) {
         $this->resourceConnection = $resourceConnection;
         $this->request = $request;
+        $this->scopeConfig = $scopeConfig;
     }
 
     public function aroundLoad(
@@ -24,8 +31,12 @@ class SearchOverride
         $logQuery = false
     ) {
         $searchQuery = $this->request->getParam('q'); // Get the search query parameter
+        $isEnabled = $this->scopeConfig->getValue(
+            'gopersonal/general/gopersonal_has_search',
+            ScopeInterface::SCOPE_STORE
+        );
 
-        if ($searchQuery) {
+        if ($searchQuery && $isEnabled === 'YES') {
             $fixedProductIds = [1556]; // Your array of fixed product IDs (or fetch dynamically)
 
             if (!empty($fixedProductIds)) {
