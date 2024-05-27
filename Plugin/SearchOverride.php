182a8f6ad0fa5c957116b74cd7f9a402cac4986a
Santiago Cotto <scotto@goshops.ai>
2024-05-27 13:36:05 -0300

    asd

--- Plugin/SearchOverride.php
+++ Plugin/SearchOverride.php
@@ -1,33 +1,29 @@
 <?php
-namespace Gopersonal\Magento\Plugin; // Adjust the namespace if needed
+namespace Gopersonal\Magento\Plugin; 
 
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection as SearchCollection;
 use Magento\Framework\App\ResourceConnection;
 
 class SearchOverride
 {
-    protected $fixedProductIds;
     protected $resourceConnection;
 
-    public function __construct(
-        array $fixedProductIds = [],
-        ResourceConnection $resourceConnection
-    ) {
-        $this->fixedProductIds = [123, 456, 789];
+    public function __construct(ResourceConnection $resourceConnection) 
+    {
         $this->resourceConnection = $resourceConnection;
     }
 
-    public function aroundGetSelect(
-        SearchCollection $subject,
-        \Closure $proceed
-    ) {
+    public function aroundGetSelect(SearchCollection $subject, \Closure $proceed) 
+    {
         $select = $proceed();
 
-        if (!empty($this->fixedProductIds)) {
-            $connection = $this->resourceConnection->getConnection();
-            $unionSelect = $connection->select()
-                ->from(['fixed_products' => $subject->getMainTable()], null) // Assuming product flat table is the main table
-                ->where('fixed_products.entity_id IN (?)', $this->fixedProductIds);
+        $fixedProductIds = [123, 456, 789];
+
+        if (!empty($fixedProductIds)) { // Removed the $this-> from fixedProductIds
+            $unionSelect = $this->resourceConnection->getConnection()->select()
+                ->from(['fixed_products' => $subject->getMainTable()])
+                ->where('fixed_products.entity_id IN (?)', $fixedProductIds); // Removed the $this-> from fixedProductIds
+
             $select->union([$unionSelect]);
         }
 
