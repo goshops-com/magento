3f17e9eb0771083a06858483cd1ab1e0d8791798
Santiago Cotto <scotto@goshops.ai>
2024-05-27 14:45:34 -0300

    fixes

--- Plugin/SearchOverride.php
+++ Plugin/SearchOverride.php
@@ -1,55 +1,59 @@
 <?php
 
-namespace Gopersonal\Magento\Plugin;
+namespace Gopersonal\Magento\Helper;
 
-use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection as SearchCollection;
-use Magento\Framework\App\ResourceConnection;
-use Magento\Framework\App\Request\Http;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
-use Magento\Framework\Stdlib\CookieManagerInterface;
+use Magento\Framework\HTTP\Client\Curl;
+use Psr\Log\LoggerInterface;
 
-class SearchOverride
+class Data extends AbstractHelper
 {
-    protected $resourceConnection;
-    protected $request;
     protected $scopeConfig;
-    protected $cookieManager;
+    protected $httpClient;
+    protected $logger;
 
     public function __construct(
-        ResourceConnection $resourceConnection,
-        Http $request,
+        Context $context,
         ScopeConfigInterface $scopeConfig,
-        CookieManagerInterface $cookieManager
+        Curl $httpClient,
+        LoggerInterface $logger
     ) {
-        $this->resourceConnection = $resourceConnection;
-        $this->request = $request;
+        parent::__construct($context);
         $this->scopeConfig = $scopeConfig;
-        $this->cookieManager = $cookieManager;
+        $this->httpClient = $httpClient;
+        $this->logger = $logger;
     }
 
-    public function aroundLoad(
-        SearchCollection $subject,
-        \Closure $proceed,
-        $printQuery = false,
-        $logQuery = false
-    ) {
-        $searchQuery = $this->request->getParam('q'); // Get the search query parameter
-        $isEnabled = $this->scopeConfig->getValue(
-            'gopersonal/general/gopersonal_has_search',
-            ScopeInterface::SCOPE_STORE
-        );
-        $token = $this->cookieManager->getCookie('gopersonal_jwt'); // Get the token from the cookie
+    public function getProductIds($query, $token)
+    {
+        try {
+            $clientId = $this->scopeConfig->getValue('gopersonal/general/client_id', ScopeInterface::SCOPE_STORE);
+            $url = 'https://discover.gopersonal.ai/item/search?adapter=magento';
 
-        if ($searchQuery && $isEnabled === 'YES' && $token !== null) {
-            $fixedProductIds = [123, 456, 789]; // Your array of fixed product IDs (or fetch dynamically)
-
-            if (!empty($fixedProductIds)) {
-                $subject->getSelect()->reset(\Zend_Db_Select::WHERE);
-                $subject->getSelect()->where('e.entity_id IN (?)', $fixedProductIds);
+            if (strpos($clientId, 'D-') === 0) {
+                $url = 'https://go-discover-dev.goshops.ai/item/search?adapter=magento';
             }
-        }
 
-        return $proceed($printQuery, $logQuery); // Let the original load method proceed with the modified query
+            $url .= '&query=' . urlencode($query);
+
+            $this->httpClient->addHeader("Authorization", "Bearer " . $token);
+            $this->httpClient->get($url);
+            $response = $this->httpClient->getBody();
+
+            $productIds = json_decode($response, true);
+
+            if (json_last_error() === JSON_ERROR_NONE && is_array($productIds)) {
+                return $productIds;
+            } else {
+                $this->logger->error('Invalid JSON response: ' . json_last_error_msg());
+                return [];
+            }
+        } catch (\Exception $e) {
+            $this->logger->error('Error fetching product IDs: ' . $e->getMessage());
+            return [];
+        }
     }
 }
