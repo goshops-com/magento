8d60091f90ae95176c403091daa582ace65aee45
Santiago Cotto <scotto@goshops.ai>
2024-05-27 13:03:11 -0300

    asd

---
+++ Plugin/SearchOverride.php
@@ -0,0 +1,27 @@
+<?php
+namespace Gopersonal\Magento\Plugin; // Adjust the namespace if needed
+
+use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection as SearchCollection;
+
+class SearchOverride
+{
+    protected $fixedProductIds;
+
+    public function __construct(
+        array $fixedProductIds = [] // Your array of fixed product IDs
+    ) {
+        $this->fixedProductIds = $fixedProductIds;
+    }
+
+    public function aroundGetSelect(
+        SearchCollection $subject, 
+        \Closure $proceed
+    ) {
+        if (!empty($this->fixedProductIds)) {
+            $select = $proceed();
+            $select->where('e.entity_id IN (?)', $this->fixedProductIds);
+            return $select;
+        }
+        return $proceed();
+    }
+}
