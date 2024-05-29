382d7872d1bc84d14fd56d8e3a5daba4236fcb78
Santiago Cotto <scotto@goshops.ai>
2024-05-29 09:58:15 -0300

    asd

--- Plugin/SearchOverride.php
+++ Plugin/SearchOverride.php
@@ -4,14 +4,24 @@ namespace Gopersonal\Magento\Plugin;
 
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection as SearchCollection;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class SearchOverride
 {
     protected $resourceConnection;
+    protected $request;
+    protected $scopeConfig;
 
-    public function __construct(ResourceConnection $resourceConnection)
-    {
+    public function __construct(
+        ResourceConnection $resourceConnection,
+        Http $request,
+        ScopeConfigInterface $scopeConfig
+    ) {
         $this->resourceConnection = $resourceConnection;
+        $this->request = $request;
+        $this->scopeConfig = $scopeConfig;
     }
 
     public function aroundLoad(
@@ -20,10 +30,19 @@ class SearchOverride
         $printQuery = false,
         $logQuery = false
     ) {
-        $fixedProductIds = [1556]; // Your array of fixed product IDs (or fetch dynamically)
+        $searchQuery = $this->request->getParam('q'); // Get the search query parameter
+        $isEnabled = $this->scopeConfig->getValue(
+            'gopersonal/general/gopersonal_has_search',
+            ScopeInterface::SCOPE_STORE
+        );
 
-        if (!empty($fixedProductIds)) {
-            $subject->getSelect()->orWhere('e.entity_id IN (?)', $fixedProductIds);
+        if ($searchQuery && $isEnabled === 'YES') {
+            $fixedProductIds = [1556]; // Your array of fixed product IDs (or fetch dynamically)
+
+            if (!empty($fixedProductIds)) {
+                $subject->getSelect()->reset(\Zend_Db_Select::WHERE);
+                $subject->getSelect()->where('e.entity_id IN (?)', $fixedProductIds);
+            }
         }
 
         return $proceed($printQuery, $logQuery); // Let the original load method proceed with the modified query
