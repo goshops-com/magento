8e9f7d102df349cf2da2dbc9d2a7f21fdaf97acd
Santiago Cotto <scotto@goshops.ai>
2024-05-30 08:06:40 -0300

    ad

--- Model/CustomSearch.php
+++ Model/CustomSearch.php
@@ -5,27 +5,33 @@ namespace Gopersonal\Magento\Model;
 use Magento\Framework\Api\Search\SearchResultFactory;
 use Magento\Search\Model\SearchEngine;
 use Magento\Framework\Api\SearchCriteriaInterface;
-use Magento\Framework\Api\Search\SearchResultInterfaceFactory;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
 
 class CustomSearch
 {
     protected $searchEngine;
     protected $searchResultFactory;
+    protected $productCollectionFactory;
 
     public function __construct(
         SearchEngine $searchEngine,
-        SearchResultFactory $searchResultFactory
+        SearchResultFactory $searchResultFactory,
+        ProductCollectionFactory $productCollectionFactory
     ) {
         $this->searchEngine = $searchEngine;
         $this->searchResultFactory = $searchResultFactory;
+        $this->productCollectionFactory = $productCollectionFactory;
     }
 
     public function search(SearchCriteriaInterface $searchCriteria)
     {
         // Create a hardcoded search result with product ID 1556
         $searchResult = $this->searchResultFactory->create();
-        $searchResult->setItems([$this->createHardcodedProduct()]);
-        $searchResult->setTotalCount(1);
+        $productCollection = $this->productCollectionFactory->create();
+        $productCollection->addIdFilter([1556]);
+        
+        $searchResult->setItems($productCollection->getItems());
+        $searchResult->setTotalCount($productCollection->getSize());
 
         return $searchResult;
     }
@@ -34,10 +40,5 @@ class CustomSearch
     {
         // Call the search method for consistency
         return $this->search($searchCriteria);
-    }
-
-    protected function createHardcodedProduct()
-    {
-        return new \Magento\Framework\DataObject(['entity_id' => 1556]);
     }
 }
