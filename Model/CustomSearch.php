5002f61d173efdf49c2ef105e9bb45531dd45926
Santiago Cotto <scotto@goshops.ai>
2024-05-30 08:22:03 -0300

    asd

--- Model/CustomSearch.php
+++ Model/CustomSearch.php
@@ -2,6 +2,7 @@
 
 namespace Gopersonal\Magento\Model;
 
+use Psr\Log\LoggerInterface;
 use Magento\Framework\Api\Search\SearchResultFactory;
 use Magento\Search\Model\SearchEngine;
 use Magento\Framework\Api\SearchCriteriaInterface;
@@ -12,32 +13,50 @@ class CustomSearch
     protected $searchEngine;
     protected $searchResultFactory;
     protected $productCollectionFactory;
+    protected $logger;
 
     public function __construct(
         SearchEngine $searchEngine,
         SearchResultFactory $searchResultFactory,
-        ProductCollectionFactory $productCollectionFactory
+        ProductCollectionFactory $productCollectionFactory,
+        LoggerInterface $logger
     ) {
         $this->searchEngine = $searchEngine;
         $this->searchResultFactory = $searchResultFactory;
         $this->productCollectionFactory = $productCollectionFactory;
+        $this->logger = $logger;
     }
 
     public function search(SearchCriteriaInterface $searchCriteria)
     {
+        $this->logger->info('CustomSearch: search method called');
+        
         // Create a hardcoded search result with product ID 1556
         $searchResult = $this->searchResultFactory->create();
+        $this->logger->info('CustomSearch: SearchResultFactory created');
+        
+        $searchResult->setSearchCriteria($searchCriteria);
+        $this->logger->info('CustomSearch: SearchCriteria set');
+
         $productCollection = $this->productCollectionFactory->create();
+        $this->logger->info('CustomSearch: ProductCollectionFactory created');
+        
         $productCollection->addIdFilter([1556]);
+        $this->logger->info('CustomSearch: ProductCollection filtered with ID 1556');
         
         $searchResult->setItems($productCollection->getItems());
+        $this->logger->info('CustomSearch: Items set in SearchResult');
+
         $searchResult->setTotalCount($productCollection->getSize());
+        $this->logger->info('CustomSearch: TotalCount set in SearchResult');
 
         return $searchResult;
     }
 
     public function getList(SearchCriteriaInterface $searchCriteria)
     {
+        $this->logger->info('CustomSearch: getList method called');
+        
         // Call the search method for consistency
         return $this->search($searchCriteria);
     }
--- Plugin/LayeredNavigationPlugin.php
+++ Plugin/LayeredNavigationPlugin.php
@@ -2,6 +2,7 @@
 
 namespace Gopersonal\Magento\Plugin;
 
+use Psr\Log\LoggerInterface;
 use Magento\Catalog\Model\Layer\Resolver as LayerResolver;
 use Magento\Framework\App\RequestInterface;
 
@@ -9,23 +10,35 @@ class LayeredNavigationPlugin
 {
     protected $layerResolver;
     protected $request;
+    protected $logger;
 
-    public function __construct(LayerResolver $layerResolver, RequestInterface $request)
+    public function __construct(LayerResolver $layerResolver, RequestInterface $request, LoggerInterface $logger)
     {
         $this->layerResolver = $layerResolver;
         $this->request = $request;
+        $this->logger = $logger;
     }
 
     public function aroundGetProductCollection($subject, callable $proceed)
     {
+        $this->logger->info('LayeredNavigationPlugin: aroundGetProductCollection called');
+        
         // Proceed with the original method
         $productCollection = $proceed();
+        $this->logger->info('LayeredNavigationPlugin: original getProductCollection called');
 
         if ($this->isCategoryPage()) {
+            $this->logger->info('LayeredNavigationPlugin: isCategoryPage is true');
+            
             // Ensure the layered navigation works on category pages
             $layer = $this->layerResolver->get();
+            $this->logger->info('LayeredNavigationPlugin: LayerResolver get called');
+
             $layer->apply();
+            $this->logger->info('LayeredNavigationPlugin: Layer apply called');
+
             $productCollection = $layer->getProductCollection();
+            $this->logger->info('LayeredNavigationPlugin: Layer getProductCollection called');
         }
 
         return $productCollection;
@@ -33,6 +46,8 @@ class LayeredNavigationPlugin
 
     protected function isCategoryPage()
     {
-        return $this->request->getFullActionName() === 'catalog_category_view';
+        $isCategoryPage = $this->request->getFullActionName() === 'catalog_category_view';
+        $this->logger->info('LayeredNavigationPlugin: isCategoryPage: ' . ($isCategoryPage ? 'true' : 'false'));
+        return $isCategoryPage;
     }
 }
