1c6b2f265bd2a3c90fb8bc19cb40e5e2129b64b7
Santiago Cotto <scotto@goshops.ai>
2024-05-31 17:36:46 -0300

    asd

--- Helper/Data.php
+++ Helper/Data.php
@@ -3,13 +3,30 @@
 namespace Gopersonal\Magento\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
-use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Request\Http;
+use Psr\Log\LoggerInterface;
 
 class Data extends AbstractHelper
 {
+    protected $request;
+    protected $logger;
+
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        Http $request,
+        LoggerInterface $logger
+    ) {
+        $this->request = $request;
+        $this->logger = $logger;
+        parent::__construct($context);
+    }
 
     public function getProductsIds()
     {
+        $q = $this->request->getParam('q', '');
+        if ($q) {
+            $this->logger->info('Search query parameter:', ['q' => $q]);
+        }
         $idArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
         return $idArray;
     }
--- Observer/RedirectSearch.php
+++ Observer/RedirectSearch.php
@@ -10,25 +10,41 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\App\ActionFlag;
 use Magento\Framework\App\Response\RedirectInterface;
 use Magento\Framework\UrlInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class RedirectSearch implements ObserverInterface
 {
     protected $actionFlag;
     protected $redirect;
     protected $url;
+    protected $scopeConfig;
 
     public function __construct(
         ActionFlag $actionFlag,
         RedirectInterface $redirect,
-        UrlInterface $url
+        UrlInterface $url,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->actionFlag = $actionFlag;
         $this->redirect = $redirect;
         $this->url = $url;
+        $this->scopeConfig = $scopeConfig;
     }
 
     public function execute(Observer $observer)
     {
+        // Check if the feature is enabled
+        $isEnabled = $this->scopeConfig->getValue(
+            'gopersonal/general/gopersonal_has_search',
+            ScopeInterface::SCOPE_STORE
+        );
+
+        if ($isEnabled !== 'YES') {
+            // Skip the redirect if the feature is not enabled
+            return;
+        }
+
         $controller = $observer->getControllerAction();
         $request = $controller->getRequest();
         $queryParams = $request->getParams();
