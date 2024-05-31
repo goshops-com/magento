91ca2d0684d897a137945645c5c8a5131ad20695
Santiago Cotto <scotto@goshops.ai>
2024-05-31 15:58:02 -0300

    fixes

---
+++ Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+// app/code/Gopersonal/Search/Helper/Data.php
+
+namespace Gopersonal\Magento\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+
+    public function getProductsIds()
+    {
+        $idArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        return $idArray;
+    }
+}
