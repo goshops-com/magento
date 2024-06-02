90408b62a2217a49ad435cbe26830de61b7f1a79
Santiago Cotto <scotto@goshops.ai>
2024-06-02 15:38:22 -0300

    asd

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -6,29 +6,37 @@ use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\RawFactory;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\View\Result\PageFactory;
+use Psr\Log\LoggerInterface; // Add the LoggerInterface
 
 class Index extends Action
 {
     protected $resultRawFactory;
     protected $directoryList;
     protected $pageFactory;
+    protected $logger; // Add the logger property
 
     public function __construct(
         Context $context,
         RawFactory $resultRawFactory,
         DirectoryList $directoryList,
-        PageFactory $pageFactory
+        PageFactory $pageFactory,
+        LoggerInterface $logger // Inject the logger
     ) {
         parent::__construct($context);
         $this->resultRawFactory = $resultRawFactory;
         $this->directoryList = $directoryList;
         $this->pageFactory = $pageFactory;
+        $this->logger = $logger; // Assign the logger
     }
 
     public function execute()
     {
+        // Log entry into the execute method
+        $this->logger->info('Entered execute method');
+
         // Get the front name from the request
         $frontName = $this->getRequest()->getFrontName();
+        $this->logger->info('Front name: ' . $frontName);
 
         // Custom logic for specific front names
         if ($frontName === 'gp-firebase') {
@@ -36,15 +44,19 @@ class Index extends Action
 
             $moduleDir = $this->directoryList->getPath('app');
             $filePath = $moduleDir . '/code/Gopersonal/Magento/web/gp-firebase.js';
+            $this->logger->info('File path: ' . $filePath);
 
             $jsContent = file_get_contents($filePath);
+            $this->logger->info('JS Content: ' . substr($jsContent, 0, 100) . '...'); // Log the beginning of the content
 
             $resultRaw->setContents($jsContent);
-            $resultRaw->setHeader('Content-Type', 'text/javascript'); 
+            $resultRaw->setHeader('Content-Type', 'text/javascript');
+            $this->logger->info('Returning JS content');
             return $resultRaw;
         }
 
         // Default behavior for other front names or general case
+        $this->logger->info('Returning default page factory');
         return $this->pageFactory->create();
     }
 }
