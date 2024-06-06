c5488fe55a5d2abf126b7e0be310bdd0b9edace0
Santiago Cotto <scotto@goshops.ai>
2024-06-05 22:29:07 -0300

    ads

--- Model/ResourceModel/Category.php
+++ Model/ResourceModel/Category.php
@@ -5,16 +5,26 @@
  */
 namespace Gopersonal\Magento\Model\ResourceModel;
 
+use Magento\Framework\App\ObjectManager;
+
 class Category extends \Magento\Catalog\Model\ResourceModel\Category
 {
     public function getProductCount($category)
     {
         $collection = $category->getProductCollection();
         //add custom filter
-		$idArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+		$idArray = getProductsIds();
 		if (!empty($idArray)) {
             $collection->addAttributeToFilter('entity_id', ["in"=>$idArray]);
         }
         return intval($collection->count());
     }
+
+    public function getProductsIds()
+	{
+		 $objectManager = ObjectManager::getInstance();
+		$helper = $objectManager->get(\Gopersonal\Magento\Helper\Data::class);
+
+		return $helper->getProductsIds('layer');
+	}
 }
