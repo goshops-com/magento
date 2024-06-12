a79367eeae88b83f1ea45ffc252503370adee43c
Santiago Cotto <scotto@goshops.ai>
2024-06-12 11:35:37 -0300

    fixess

--- Block/Navigation/FilterRenderer.php
+++ Block/Navigation/FilterRenderer.php
@@ -5,6 +5,7 @@ namespace Gopersonal\Magento\Block\Navigation;
 use Magento\Catalog\Model\Layer\Filter\FilterInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\LayeredNavigation\Block\Navigation\FilterRendererInterface;
+use Magento\Framework\DataObject;
 
 /**
  * Custom Catalog layer filter renderer
@@ -17,17 +18,39 @@ class FilterRenderer extends \Magento\LayeredNavigation\Block\Navigation\FilterR
      * @param FilterInterface $filter
      * @return string
      */
-    public function render(FilterInterface $filter) // Access $filter here
+    public function render(FilterInterface $filter)
     {
-        // ... (your custom logic)
-
-        // Pass the entire $filter object to the template
+        $filterItems = $this->_buildFilterItems($filter->getItems(), $filter);
+        if ($filterItems === null) {
+            $filterItems = [];
+        }
         $this->assign('filterItems', $filter->getItems());
-        $this->assign('currentFilter', $filter); // Pass the filter object
+        $this->assign('filter', $filter); // Pass the filter object
         $html = $this->_toHtml();
         $this->assign('filterItems', []);
-        $this->assign('currentFilter', null); // Unset after rendering
         return $html;
+    }
+
+    /**
+     * Build filter items manually
+     *
+     * @param array $filterItems
+     * @param FilterInterface $filter
+     * @return array
+     */
+    protected function _buildFilterItems($filterItems, $filter)
+    {
+        $result = [];
+        foreach ($filterItems as $item) {
+            if (is_object($item)) {
+                $result[] = new DataObject([
+                    'count' => $item->getCount(),
+                    'label' => $item->getLabel(),
+                    'url' => $item->getUrl(),
+                ]);
+            }
+        }
+        return $result;
     }
 
     public function getVersion()
