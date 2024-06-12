b0c5d4a082ec098c020f29da2b4a78ded0edba6e
Santiago Cotto <scotto@goshops.ai>
2024-06-12 11:37:38 -0300

    ad

--- Block/Navigation/FilterRenderer.php
+++ Block/Navigation/FilterRenderer.php
@@ -5,52 +5,15 @@ namespace Gopersonal\Magento\Block\Navigation;
 use Magento\Catalog\Model\Layer\Filter\FilterInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\LayeredNavigation\Block\Navigation\FilterRendererInterface;
-use Magento\Framework\DataObject;
 
-/**
- * Custom Catalog layer filter renderer
- */
 class FilterRenderer extends \Magento\LayeredNavigation\Block\Navigation\FilterRenderer
 {
-    /**
-     * Render filter
-     *
-     * @param FilterInterface $filter
-     * @return string
-     */
     public function render(FilterInterface $filter)
     {
-        $filterItems = $this->_buildFilterItems($filter->getItems(), $filter);
-        if ($filterItems === null) {
-            $filterItems = [];
-        }
         $this->assign('filterItems', $filter->getItems());
         $this->assign('filter', $filter); // Pass the filter object
         $html = $this->_toHtml();
-        $this->assign('filterItems', []);
         return $html;
-    }
-
-    /**
-     * Build filter items manually
-     *
-     * @param array $filterItems
-     * @param FilterInterface $filter
-     * @return array
-     */
-    protected function _buildFilterItems($filterItems, $filter)
-    {
-        $result = [];
-        foreach ($filterItems as $item) {
-            if (is_object($item)) {
-                $result[] = new DataObject([
-                    'count' => $item->getCount(),
-                    'label' => $item->getLabel(),
-                    'url' => $item->getUrl(),
-                ]);
-            }
-        }
-        return $result;
     }
 
     public function getVersion()
