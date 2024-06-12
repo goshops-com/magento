f426cffbcaaa91a89268d41f741fd6ff8d717cf8
Santiago Cotto <scotto@goshops.ai>
2024-06-12 13:43:31 -0300

    asd

--- Model/Layer.php
+++ Model/Layer.php
@@ -41,48 +41,30 @@ class Layer extends \Magento\Catalog\Model\Layer
 
 	private function calculateFilterCounts(\Magento\Catalog\Model\ResourceModel\Product\Collection $collection)
 	{
-		$filterCounts = [];
-		$logger = \Magento\Framework\App\ObjectManager::getInstance()->get(\Psr\Log\LoggerInterface::class);
+    $filterCounts = [];
+    $logger = ObjectManager::getInstance()->get(\Psr\Log\LoggerInterface::class);
 
-		$logger->info('Starting filter count calculation'); 
-		
-		foreach ($this->getState()->getFilters() as $filter) {
-			$attribute = $filter->getFilter()->getAttributeModel();
-			$attributeCode = $attribute->getAttributeCode();
-			$logger->debug('Calculating counts for attribute: ' . $attributeCode); 
+    $logger->info('Starting filter count calculation'); 
 
-			$filterCounts[$attributeCode] = [];
+    foreach ($this->getState()->getFilters() as $filter) {
+        $attribute = $filter->getFilter()->getAttributeModel();
+        $attributeCode = $attribute->getAttributeCode();
+        $logger->debug('Calculating counts for attribute: ' . $attributeCode); 
 
-			// Get all possible filter options (not just applied ones)
-			$allFilterOptions = $attribute->getSource()->getAllOptions();
-			
-			// Initialize counts for all options
-			foreach ($allFilterOptions as $option) {
-				$filterCounts[$attributeCode][$option['value']] = 0; 
-			}
+        $filterItems = $filter->getFilter()->getItems() ?: []; // Default to empty array if null
 
-			// Iterate over filter items and update counts from filtered collection
-			$filterItems = $filter->getFilter()->getItems() ?: []; // Default to empty array if null
+        foreach ($filterItems as $filterItem) {
+            $value = $filterItem->getValue();
+            $count = $collection->addFieldToFilter($attributeCode, $value)->getSize();
+            $filterCounts[$attributeCode][$value] = $count;
+            $logger->debug('Filter item "' . $filterItem->getLabel() . '" (' . $value . ') has count: ' . $count);
+        }
 
-			foreach ($filterItems as $filterItem) {
-				$value = $filterItem->getValue();
-				$count = $collection->addFieldToFilter($attributeCode, $value)->getSize();
-				$filterCounts[$attributeCode][$value] = $count;
-				$logger->debug('Filter item "' . $filterItem->getLabel() . '" (' . $value . ') has count: ' . $count);
-			}
+    }
 
-			// If an option doesn't have a product, we remove it from filterCounts
-			foreach ($filterCounts[$attributeCode] as $optionKey => $optionValue) {
-			if ($optionValue == 0){
-				unset($filterCounts[$attributeCode][$optionKey]);
-				$logger->debug('Removing filter item with no products: ' . $optionKey);
-			}
-			}
-		}
+    $logger->info('Finished filter count calculation');
 
-		$logger->info('Finished filter count calculation');
-
-		return $filterCounts;
+    return $filterCounts;
 	}
 
 }
