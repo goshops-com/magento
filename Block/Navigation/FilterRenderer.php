8d830220ac97b21973a68760124b5d25acd0f1a5
Santiago Cotto <scotto@goshops.ai>
2024-06-13 09:34:31 -0300

    fixes

--- Block/Navigation/FilterRenderer.php
+++ Block/Navigation/FilterRenderer.php
@@ -3,30 +3,34 @@ namespace Gopersonal\Magento\Block\Navigation;
 
 use Magento\Framework\View\Element\Template;
 use Psr\Log\LoggerInterface;
+use Magento\Framework\App\RequestInterface;
 
 class FilterRenderer extends \Magento\LayeredNavigation\Block\Navigation\FilterRenderer
 {
     protected $logger;
+    protected $request;
 
     public function __construct(
         Template\Context $context,
         LoggerInterface $logger,
+        RequestInterface $request,
         array $data = []
     ) {
         $this->logger = $logger;
+        $this->request = $request;
         parent::__construct($context, $data);
     }
 
     public function getFilterCounts()
     {
-        $filterCounts = $this->getData('filter_counts');
+        $filterCounts = $this->request->getParam('filter_counts');
         $this->logger->info('Filter Counts: from Render ' . json_encode($filterCounts));
         return $filterCounts;
     }
 
     public function getFilterData()
     {
-        $filter = $this->getData('filter');
+        $filter = $this->request->getParam('filter');
         $this->logger->info('Filter Data: from Render ' . json_encode($filter));
         return $filter;
     }
