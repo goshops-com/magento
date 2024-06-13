1a3f4be2fea48913a14c33904f325552e97ab671
Santiago Cotto <scotto@goshops.ai>
2024-06-13 09:24:56 -0300

    sad

--- Block/Navigation/FilterRenderer.php
+++ Block/Navigation/FilterRenderer.php
@@ -1,25 +1,43 @@
 <?php
-
 namespace Gopersonal\Magento\Block\Navigation;
 
-use Magento\Catalog\Model\Layer\Filter\FilterInterface;
 use Magento\Framework\View\Element\Template;
-use Magento\LayeredNavigation\Block\Navigation\FilterRendererInterface;
+use Psr\Log\LoggerInterface;
 
 class FilterRenderer extends \Magento\LayeredNavigation\Block\Navigation\FilterRenderer
 {
-    public function render(FilterInterface $filter)
-    {
-        $this->assign('filterItems', $filter->getItems());
-        $this->assign('filter', $filter); // Pass the filter object
-        $html = $this->_toHtml();
-        return $html;
+    protected $logger;
+
+    public function __construct(
+        Template\Context $context,
+        LoggerInterface $logger,
+        array $data = []
+    ) {
+        $this->logger = $logger;
+        parent::__construct($context, $data);
     }
 
-    public function getVersion()
+    public function getFilterCounts()
     {
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $productMetadata = $objectManager->get('Magento\Framework\App\ProductMetadataInterface');
-        return $productMetadata->getVersion();
+        $filterCounts = $this->getData('filter_counts');
+        $this->logger->info('Filter Counts: from Render ' . json_encode($filterCounts));
+        return $filterCounts;
+    }
+
+    public function getFilterData()
+    {
+        $filter = $this->getData('filter');
+        $this->logger->info('Filter Data: from Render ' . json_encode($filter));
+        return $filter;
+    }
+
+    public function logFilterItem($filterItem, $filterCount)
+    {
+        $this->logger->info('Filter Item: ' . json_encode([
+            'label' => $filterItem->getLabel(),
+            'value' => $filterItem->getValue(),
+            'url' => $filterItem->getUrl(),
+            'count' => $filterCount
+        ]));
     }
 }
