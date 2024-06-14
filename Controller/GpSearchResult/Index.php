86f7debb543951a8056763212e1a9a0a36ddff06
Santiago Cotto <scotto@goshops.ai>
2024-06-13 23:42:34 -0300

    asd

---
+++ Controller/GpSearchResult/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Gopersonal\Magento\Controller\GpSearchResult;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
