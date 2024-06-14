87c5833ae45ead32ccc19f9c404ae0584f2cb4cf
Santiago Cotto <scotto@goshops.ai>
2024-06-13 23:54:25 -0300

    asd

--- Controller/GpSearchResult/Index.php
+++ Controller/GpSearchResult/Index.php
@@ -5,19 +5,23 @@ namespace Gopersonal\Magento\Controller\GpSearchResult;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
+use Psr\Log\LoggerInterface;
 
 class Index extends Action
 {
     protected $resultPageFactory;
+    protected $logger;
 
-    public function __construct(Context $context, PageFactory $resultPageFactory)
+    public function __construct(Context $context, PageFactory $resultPageFactory, LoggerInterface $logger)
     {
         $this->resultPageFactory = $resultPageFactory;
+        $this->logger = $logger;
         parent::__construct($context);
     }
 
     public function execute()
     {
+        $this->logger->info('GpSearchResult Index execute called');
         return $this->resultPageFactory->create();
     }
 }
