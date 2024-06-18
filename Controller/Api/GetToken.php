766735e5bccb0461ebcdcfb6c29d20d6828dfd68
Santiago Cotto <scotto@goshops.ai>
2024-06-18 10:18:15 -0300

    ver

--- Controller/Api/GetToken.php
+++ Controller/Api/GetToken.php
@@ -44,7 +44,7 @@ class GetToken extends Action
         $data = [
             'token' => $token ? $token : 'No token is stored.',
             'readFrom' => $readFromCookie ? 'cookie' : 'session',
-            'version' => '1.0.10'
+            'version' => '1.0.11'
         ];
 
         // Check if the customer is logged in
