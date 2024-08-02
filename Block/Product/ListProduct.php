ee08cd21884871b9bfa8e18177a85f9d14d6a3d2
Santiago Cotto <scotto@goshops.ai>
2024-08-02 13:08:37 -0300

    fixes

--- Block/Product/ListProduct.php
+++ Block/Product/ListProduct.php
@@ -24,4 +24,34 @@ class ListProduct extends \Magento\Catalog\Block\Product\ListProduct
 			$data
 		);
 	}
+
+	public function getProductUrl($product)
+    {
+        $url = $product->getProductUrl();
+        
+        // Normalize the URL
+        $url = $this->normalizeUrl($url);
+
+        return $url;
+    }
+
+    /**
+     * Normalize the URL to ensure single leading slash and no trailing slashes
+     *
+     * @param string $url
+     * @return string
+     */
+    protected function normalizeUrl($url)
+    {
+        // Remove all leading slashes
+        $url = ltrim($url, '/');
+
+        // Remove all trailing slashes
+        $url = rtrim($url, '/');
+
+        // Add a single leading slash
+        $url = '/' . $url;
+
+        return $url;
+    }
 }
