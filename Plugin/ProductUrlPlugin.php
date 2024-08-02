99eb7618399ad2639eeac1cab35f0dd7e4d12205
Santiago Cotto <scotto@goshops.ai>
2024-08-02 14:19:26 -0300

    asd

--- Plugin/ProductUrlPlugin.php
+++ Plugin/ProductUrlPlugin.php
@@ -5,7 +5,27 @@ class ProductUrlPlugin
 {
     public function afterGetProductUrl(\Magento\Catalog\Model\Product $subject, $result)
     {
-        $separator = (strpos($result, '?') !== false) ? '&' : '?';
-        return $result . $separator . 'my_param=value';
+        // Parse the URL
+        $urlParts = parse_url($result);
+        
+        if (isset($urlParts['path'])) {
+            // Remove any double slashes in the path
+            $urlParts['path'] = preg_replace('#/{2,}#', '/', $urlParts['path']);
+        }
+        
+        // Rebuild the URL
+        $scheme   = isset($urlParts['scheme']) ? $urlParts['scheme'] . '://' : '';
+        $host     = isset($urlParts['host']) ? $urlParts['host'] : '';
+        $port     = isset($urlParts['port']) ? ':' . $urlParts['port'] : '';
+        $user     = isset($urlParts['user']) ? $urlParts['user'] : '';
+        $pass     = isset($urlParts['pass']) ? ':' . $urlParts['pass']  : '';
+        $pass     = ($user || $pass) ? "$pass@" : '';
+        $path     = isset($urlParts['path']) ? $urlParts['path'] : '';
+        $query    = isset($urlParts['query']) ? '?' . $urlParts['query'] : '';
+        $fragment = isset($urlParts['fragment']) ? '#' . $urlParts['fragment'] : '';
+        
+        $cleanedUrl = $scheme . $user . $pass . $host . $port . $path . $query . $fragment;
+        
+        return $cleanedUrl;
     }
 }
