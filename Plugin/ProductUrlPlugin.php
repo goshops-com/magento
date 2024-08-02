54370441a113dba96bf762ff3168e33c461acf44
Santiago Cotto <scotto@goshops.ai>
2024-08-02 14:54:06 -0300

    sd

--- Plugin/ProductUrlPlugin.php
+++ Plugin/ProductUrlPlugin.php
@@ -6,31 +6,27 @@ class ProductUrlPlugin
     public function afterGetProductUrl(\Magento\Catalog\Model\Product $subject, $result)
     {
         // Parse the URL
-
-        $separator = (strpos($result, '?') !== false) ? '&' : '?';
-        return $result . $separator . 'my_param=value';
-
-        // $urlParts = parse_url($result);
+        $urlParts = parse_url($result);
         
-        // if (isset($urlParts['path'])) {
-        //     // Remove any double slashes in the path
-        //     $urlParts['path'] = preg_replace('#/{2,}#', '/', $urlParts['path']);
-        // }
+        if (isset($urlParts['path'])) {
+            // Remove any double slashes in the path
+            $urlParts['path'] = preg_replace('#/{2,}#', '/', $urlParts['path']);
+        }
         
-        // // Rebuild the URL
-        // $scheme   = isset($urlParts['scheme']) ? $urlParts['scheme'] . '://' : '';
-        // $host     = isset($urlParts['host']) ? $urlParts['host'] : '';
-        // $port     = isset($urlParts['port']) ? ':' . $urlParts['port'] : '';
-        // $user     = isset($urlParts['user']) ? $urlParts['user'] : '';
-        // $pass     = isset($urlParts['pass']) ? ':' . $urlParts['pass']  : '';
-        // $pass     = ($user || $pass) ? "$pass@" : '';
-        // $path     = isset($urlParts['path']) ? $urlParts['path'] : '';
-        // $query    = isset($urlParts['query']) ? '?' . $urlParts['query'] : '';
-        // $fragment = isset($urlParts['fragment']) ? '#' . $urlParts['fragment'] : '';
+        // Rebuild the URL
+        $scheme   = isset($urlParts['scheme']) ? $urlParts['scheme'] . '://' : '';
+        $host     = isset($urlParts['host']) ? $urlParts['host'] : '';
+        $port     = isset($urlParts['port']) ? ':' . $urlParts['port'] : '';
+        $user     = isset($urlParts['user']) ? $urlParts['user'] : '';
+        $pass     = isset($urlParts['pass']) ? ':' . $urlParts['pass']  : '';
+        $pass     = ($user || $pass) ? "$pass@" : '';
+        $path     = isset($urlParts['path']) ? $urlParts['path'] : '';
+        $query    = isset($urlParts['query']) ? '?' . $urlParts['query'] : '';
+        $fragment = isset($urlParts['fragment']) ? '#' . $urlParts['fragment'] : '';
         
-        // $cleanedUrl = $scheme . $user . $pass . $host . $port . $path . $query . $fragment;
+        $cleanedUrl = $scheme . $user . $pass . $host . $port . $path . $query . $fragment;
         
-        // return $cleanedUrl;
+        return $cleanedUrl;
     }
 
     private function addParamToUrl($url, $paramName, $paramValue)
