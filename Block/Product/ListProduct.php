fbc9f825888d8d2e122fab9387f92b364518b8ea
Santiago Cotto <scotto@goshops.ai>
2024-08-02 14:08:54 -0300

    asd

--- Block/Product/ListProduct.php
+++ Block/Product/ListProduct.php
@@ -28,61 +28,15 @@ class ListProduct extends \Magento\Catalog\Block\Product\ListProduct
         );
     }
 
-    // /**
-    //  * Get Product URL
-    //  *
-    //  * @param Product $product
-    //  * @param array $additional
-    //  * @return string
-    //  */
-    // public function getProductUrl($product, $additional = [])
-    // {
-    //     $originalUrl = parent::getProductUrl($product, $additional);
-        
-    //     // Normalize the URL
-    //     $modifiedUrl = $this->normalizeUrl($originalUrl);
+    public function getProductUrl($product)
+    {
+        $url = parent::getProductUrl($product);
+        return $this->addParamToUrl($url, 'my_param', 'value');
+    }
 
-    //     // Log the original and modified URLs
-    //     $this->logUrls($originalUrl, $modifiedUrl);
-
-    //     return $modifiedUrl;
-    // }
-
-    // /**
-    //  * Normalize the URL to ensure no double slashes after the protocol
-    //  *
-    //  * @param string $url
-    //  * @return string
-    //  */
-    // protected function normalizeUrl($url)
-    // {
-    //     // Parse the URL to extract components
-    //     $parsedUrl = parse_url($url);
-
-    //     // Reconstruct the URL, ensuring no double slashes
-    //     $scheme = isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] . '://' : '';
-    //     $host = isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
-    //     $path = isset($parsedUrl['path']) ? $parsedUrl['path'] : '';
-
-    //     // Normalize path to remove double slashes
-    //     $path = preg_replace('#/+#', '/', $path);
-
-    //     // Construct the normalized URL
-    //     $normalizedUrl = $scheme . $host . $path;
-
-    //     return $normalizedUrl;
-    // }
-
-    // /**
-    //  * Log the original and modified URLs
-    //  *
-    //  * @param string $originalUrl
-    //  * @param string $modifiedUrl
-    //  * @return void
-    //  */
-    // protected function logUrls($originalUrl, $modifiedUrl)
-    // {
-    //     $this->logger->debug('Original Product URL: ' . $originalUrl);
-    //     $this->logger->debug('Modified Product URL: ' . $modifiedUrl);
-    // }
+    private function addParamToUrl($url, $paramName, $paramValue)
+    {
+        $separator = (strpos($url, '?') !== false) ? '&' : '?';
+        return $url . $separator . $paramName . '=' . urlencode($paramValue);
+    }
 }
