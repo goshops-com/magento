b9fe0cfc1e0a129afc988afe6aaa9e107447d252
Santiago Cotto <scotto@goshops.ai>
2024-08-02 13:26:29 -0300

    ad

--- Block/Product/ListProduct.php
+++ Block/Product/ListProduct.php
@@ -2,17 +2,22 @@
 namespace Gopersonal\Magento\Block\Product;
 
 use Magento\Catalog\Model\Product;
+use Psr\Log\LoggerInterface;
 
 class ListProduct extends \Magento\Catalog\Block\Product\ListProduct
 {
+    protected $logger;
+
     public function __construct(
         \Magento\Catalog\Block\Product\Context $context,
         \Magento\Framework\Data\Helper\PostHelper $postDataHelper,
         \Gopersonal\Magento\Model\Layer\Resolver $layerResolver,
         \Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository,
         \Magento\Framework\Url\Helper\Data $urlHelper,
+        LoggerInterface $logger,
         array $data = []
     ) {
+        $this->logger = $logger;
         parent::__construct(
             $context,
             $postDataHelper,
@@ -32,12 +37,15 @@ class ListProduct extends \Magento\Catalog\Block\Product\ListProduct
      */
     public function getProductUrl($product, $additional = [])
     {
-        $url = parent::getProductUrl($product, $additional);
+        $originalUrl = parent::getProductUrl($product, $additional);
         
         // Normalize the URL
-        $url = $this->normalizeUrl($url);
+        $modifiedUrl = $this->normalizeUrl($originalUrl);
 
-        return $url;
+        // Log the original and modified URLs
+        $this->logUrls($originalUrl, $modifiedUrl);
+
+        return $modifiedUrl;
     }
 
     /**
@@ -63,5 +71,18 @@ class ListProduct extends \Magento\Catalog\Block\Product\ListProduct
         $normalizedUrl = $scheme . $host . $path;
 
         return $normalizedUrl;
+    }
+
+    /**
+     * Log the original and modified URLs
+     *
+     * @param string $originalUrl
+     * @param string $modifiedUrl
+     * @return void
+     */
+    protected function logUrls($originalUrl, $modifiedUrl)
+    {
+        $this->logger->debug('Original Product URL: ' . $originalUrl);
+        $this->logger->debug('Modified Product URL: ' . $modifiedUrl);
     }
 }
