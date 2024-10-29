7e74e153e7ccfec164b1189aa79275fbdba45d1c
Santiago Cotto <scotto@goshops.ai>
2024-10-29 14:31:29 -0300

    sd

--- Model/Search/CustomSearchResultProvider.php
+++ Model/Search/CustomSearchResultProvider.php
@@ -1,16 +1,13 @@
 <?php
 namespace Gopersonal\Magento\Model\Search;
 
-use Magento\Framework\Api\Search\SearchResultInterface;
-use Magento\Framework\Api\SearchResultsInterface;
-use Magento\Framework\Search\Response\QueryInterface;
+use Magento\Framework\Search\Response\QueryResponse;
+use Magento\Framework\Search\Response\Aggregation;
 
-class CustomSearchResultProvider extends \Magento\Framework\Search\Response\QueryResponse 
+class CustomSearchResultProvider extends QueryResponse 
 {
     public function __construct()
     {
-        die('Custom Search Provider Called!');
-        
         $documents = [
             [
                 'entity_id' => 2040,
@@ -18,6 +15,6 @@ class CustomSearchResultProvider extends \Magento\Framework\Search\Response\Quer
             ]
         ];
         
-        parent::__construct($documents, []);
+        parent::__construct($documents, new Aggregation([]));
     }
 }
