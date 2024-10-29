b75f207dff985e63def82cbdc0f4543f594924af
Santiago Cotto <scotto@goshops.ai>
2024-10-29 13:58:23 -0300

    ads

--- Plugin/SearchPlugin.php
+++ Plugin/SearchPlugin.php
@@ -3,11 +3,18 @@ namespace Gopersonal\Magento\Plugin;
 
 class SearchPlugin
 {
-    public function beforeLoad(\Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection $collection)
-    {
-        $collection->getSelect()->reset(\Zend_Db_Select::WHERE);
-        $collection->getSelect()->where('e.entity_id IN (?)', [2040]);
-        
-        return null;
+    public function aroundLoad(
+        \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection $subject,
+        \Closure $proceed,
+        $printQuery = false,
+        $logQuery = false
+    ) {
+        // Reset the WHERE clause
+        $subject->getSelect()->reset(\Zend_Db_Select::WHERE);
+        // Add your hardcoded product IDs
+        $subject->getSelect()->where('e.entity_id IN (?)', [2040]);
+
+        // Proceed with the load
+        return $proceed($printQuery, $logQuery);
     }
 }
