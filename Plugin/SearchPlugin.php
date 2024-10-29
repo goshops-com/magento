0f202fa06fdad25d27fbb332a42a11249e440e06
Santiago Cotto <scotto@goshops.ai>
2024-10-29 13:49:07 -0300

    asd

--- Plugin/SearchPlugin.php
+++ Plugin/SearchPlugin.php
@@ -5,7 +5,9 @@ class SearchPlugin
 {
     public function beforeLoad(\Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection $collection)
     {
-        die('Search plugin called!');
+        $collection->getSelect()->reset(\Zend_Db_Select::WHERE);
+        $collection->getSelect()->where('e.entity_id IN (?)', [2040]);
+        
         return null;
     }
 }
