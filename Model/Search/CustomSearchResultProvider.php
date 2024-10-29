2998f0ffdbe9db587f9264b05c929c57d33a193d
Santiago Cotto <scotto@goshops.ai>
2024-10-29 14:49:54 -0300

    asd

--- Model/Search/CustomSearchResultProvider.php
+++ Model/Search/CustomSearchResultProvider.php
@@ -4,24 +4,18 @@ namespace Gopersonal\Magento\Model\Search;
 use Magento\Framework\Search\Response\QueryResponse;
 use Magento\Framework\Search\Response\Aggregation;
 use Magento\Framework\Api\Search\Document;
-use Magento\Framework\Api\AttributeValue;
 
 class CustomSearchResultProvider extends QueryResponse 
 {
-    public function __construct()
-    {
-        $documents = [
-            new Document(
-                '2040', 
-                [
-                    'entity_id' => new AttributeValue([
-                        'attribute_code' => 'entity_id',
-                        'value' => 2040
-                    ])
-                ]
-            )
-        ];
-        
-        parent::__construct($documents, new Aggregation([]));
-    }
+   public function __construct()
+   {
+       $documents = [
+           new Document(
+               ['entity_id' => 2040],
+               '2040'
+           )
+       ];
+       
+       parent::__construct($documents, new Aggregation([]));
+   }
 }
