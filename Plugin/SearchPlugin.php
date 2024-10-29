fa8930822b67858a0a3cf1b8a5226fef345e2c78
Santiago Cotto <scotto@goshops.ai>
2024-10-29 14:04:13 -0300

    asd

--- Plugin/SearchPlugin.php
+++ Plugin/SearchPlugin.php
@@ -3,18 +3,33 @@ namespace Gopersonal\Magento\Plugin;
 
 class SearchPlugin
 {
-    public function aroundLoad(
+    public function aroundAddSearchFilter(
         \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection $subject,
         \Closure $proceed,
-        $printQuery = false,
-        $logQuery = false
+        $query
     ) {
-        // Reset the WHERE clause
+        // Prevent the search filter from being applied
+        return $subject;
+    }
+
+    public function beforeLoad(\Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection $subject)
+    {
+        // Reset existing query conditions
         $subject->getSelect()->reset(\Zend_Db_Select::WHERE);
+        $subject->getSelect()->reset(\Zend_Db_Select::HAVING);
+        $subject->getSelect()->reset(\Zend_Db_Select::FROM);
+        $subject->getSelect()->reset(\Zend_Db_Select::GROUP);
+        $subject->getSelect()->reset(\Zend_Db_Select::JOIN);
+
+        // Rebuild the FROM clause to ensure base table is set correctly
+        $subject->getSelect()->from(
+            ['e' => $subject->getTable('catalog_product_entity')],
+            ['entity_id']
+        );
+
         // Add your hardcoded product IDs
         $subject->getSelect()->where('e.entity_id IN (?)', [2040]);
 
-        // Proceed with the load
-        return $proceed($printQuery, $logQuery);
+        return null;
     }
 }
