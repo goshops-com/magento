ec4c9ac9025a7dcb5c54592bfb66b90d991bcff1
Santiago Cotto <scotto@goshops.ai>
2024-10-29 13:07:22 -0300

    asd

--- Model/ResourceModel/Fulltext/Collection/CustomSearchResultApplier.php
+++ Model/ResourceModel/Fulltext/Collection/CustomSearchResultApplier.php
@@ -1,67 +1,21 @@
 <?php
-
 namespace Gopersonal\Magento\Model\ResourceModel\Fulltext\Collection;
 
-use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection\SearchResultApplier;
 use Magento\Framework\Api\Search\SearchResultInterface;
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection;
-use Psr\Log\LoggerInterface;
+use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection\SearchResultApplierInterface;
 
-class CustomSearchResultApplier extends SearchResultApplier
+class CustomSearchResultApplier implements SearchResultApplierInterface
 {
-    /**
-     * @var SearchResultInterface
-     */
-    private $searchResult;
-
-    /**
-     * @var Collection
-     */
-    private $collection;
-
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    /**
-     * @param SearchResultInterface $searchResult
-     * @param Collection $collection
-     * @param LoggerInterface $logger
-     */
     public function __construct(
         SearchResultInterface $searchResult,
-        Collection $collection,
-        LoggerInterface $logger
+        Collection $collection
     ) {
-        parent::__construct($searchResult, $collection);
-        $this->searchResult = $searchResult;
-        $this->collection = $collection;
-        $this->logger = $logger;
+        die('Constructor called!'); // This will show if the class is instantiated
     }
 
     public function apply()
     {
-        $this->logger->info('CustomSearchResultApplier is being called');
-        
-        // Force clear any existing where conditions
-        $this->collection->getSelect()->reset(\Zend_Db_Select::WHERE);
-        
-        // Hardcoded product ID
-        $productIds = [2040];
-        
-        // Log the SQL query before
-        $this->logger->info('Query before: ' . $this->collection->getSelect()->__toString());
-        
-        if (empty($productIds)) {
-            $this->collection->getSelect()->where('e.entity_id IN (0)');
-            return;
-        }
-
-        // Add our where condition
-        $this->collection->getSelect()->where('e.entity_id IN (?)', $productIds);
-        
-        // Log the final SQL query
-        $this->logger->info('Query after: ' . $this->collection->getSelect()->__toString());
+        die('Apply method called!'); // This will show if the method is called
     }
 }
