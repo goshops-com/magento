10f43132ba71b4001215a8d1a97fb10e70944b7b
Santiago Cotto <scotto@goshops.ai>
2024-10-29 10:55:21 -0300

    asd

--- Controller/Api/GetToken.php
+++ Controller/Api/GetToken.php
@@ -44,7 +44,7 @@ class GetToken extends Action
         $data = [
             'token' => $token ? $token : 'No token is stored.',
             'readFrom' => $readFromCookie ? 'cookie' : 'session',
-            'version' => '1.0.17'
+            'version' => '1.0.17-dev'
         ];
 
         // Check if the customer is logged in
--- Model/ResourceModel/Fulltext/Collection/CustomSearchResultApplier.php
+++ Model/ResourceModel/Fulltext/Collection/CustomSearchResultApplier.php
@@ -1,9 +1,11 @@
 <?php
+<?php
 namespace Gopersonal\Magento\Model\ResourceModel\Fulltext\Collection;
 
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection\SearchResultApplier;
 use Magento\Framework\Api\Search\SearchResultInterface;
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection;
+use Psr\Log\LoggerInterface;
 
 class CustomSearchResultApplier extends SearchResultApplier
 {
@@ -18,27 +20,48 @@ class CustomSearchResultApplier extends SearchResultApplier
     private $collection;
 
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @param SearchResultInterface $searchResult
      * @param Collection $collection
+     * @param LoggerInterface $logger
      */
     public function __construct(
         SearchResultInterface $searchResult,
-        Collection $collection
+        Collection $collection,
+        LoggerInterface $logger
     ) {
+        parent::__construct($searchResult, $collection);
         $this->searchResult = $searchResult;
         $this->collection = $collection;
+        $this->logger = $logger;
     }
 
     public function apply()
     {
+        $this->logger->info('CustomSearchResultApplier is being called');
+        
+        // Force clear any existing where conditions
+        $this->collection->getSelect()->reset(\Zend_Db_Select::WHERE);
+        
         // Hardcoded product ID
         $productIds = [2040];
+        
+        // Log the SQL query before
+        $this->logger->info('Query before: ' . $this->collection->getSelect()->__toString());
         
         if (empty($productIds)) {
             $this->collection->getSelect()->where('e.entity_id IN (0)');
             return;
         }
 
+        // Add our where condition
         $this->collection->getSelect()->where('e.entity_id IN (?)', $productIds);
+        
+        // Log the final SQL query
+        $this->logger->info('Query after: ' . $this->collection->getSelect()->__toString());
     }
 }
