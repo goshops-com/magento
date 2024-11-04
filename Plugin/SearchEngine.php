4a80179b01e6d1e1265cff58919d1ada42fb5ac0
Santiago Cotto <scotto@goshops.ai>
2024-11-04 11:23:34 -0300

    asd

--- Plugin/SearchEngine.php
+++ Plugin/SearchEngine.php
@@ -3,19 +3,23 @@ namespace Gopersonal\Magento\Plugin;
 
 use Magento\Framework\Search\ResponseInterface;
 use Magento\Framework\Search\RequestInterface;
+use Magento\Framework\Search\Document;
+use Magento\Framework\Search\Response\QueryResponse;
+use Magento\Framework\Search\Response\Aggregation;
+use Magento\Framework\Search\Response\Aggregation\Value;
 
 class SearchEngine extends \Magento\Search\Model\SearchEngine
 {
     public function search(RequestInterface $request)
     {
-        return new \Magento\Framework\Search\Response\QueryResponse(
+        return new QueryResponse(
             [
-                new \Magento\Framework\Search\Document(
+                new Document(
                     2040,
-                    ['score' => new \Magento\Framework\Search\Response\Aggregation\Value(1)]
+                    ['score' => new Value(1)]
                 )
             ],
-            new \Magento\Framework\Search\Response\Aggregation([], [])
+            new Aggregation([], [])
         );
     }
 }
