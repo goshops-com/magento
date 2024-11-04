5e4a2bc005f3aa24d489fa8bb6e866134aa3c2c4
Santiago Cotto <scotto@goshops.ai>
2024-11-04 11:56:14 -0300

    asd

--- Plugin/SearchEngine.php
+++ Plugin/SearchEngine.php
@@ -3,19 +3,35 @@ namespace Gopersonal\Magento\Plugin;
 
 use Magento\Framework\Search\RequestInterface;
 use Magento\Framework\Api\Search\Document as SearchDocument;
+use Psr\Log\LoggerInterface;
 
 class SearchEngine extends \Magento\Search\Model\SearchEngine
 {
-    public function search(RequestInterface $request)
-    {
-        return new \Magento\Framework\Search\Response\QueryResponse(
-            [
-                new SearchDocument(
-                    ['entity_id' => '2040'],
-                    ['score' => new \Magento\Framework\Search\Response\Aggregation\Value(1.0, 'value')]
-                )
-            ],
-            new \Magento\Framework\Search\Response\Aggregation([], [])
-        );
-    }
+   protected $logger;
+
+   public function __construct(
+       LoggerInterface $logger
+   ) {
+       $this->logger = $logger;
+   }
+
+   public function search(RequestInterface $request)
+   {
+       $this->logger->info('Search called with request: ' . json_encode($request->getName()));
+       $this->logger->info('Search query: ' . json_encode($request->getQuery()));
+       
+       $response = new \Magento\Framework\Search\Response\QueryResponse(
+           [
+               new SearchDocument(
+                   ['entity_id' => 1],
+                   ['score' => new \Magento\Framework\Search\Response\Aggregation\Value(1.0, 'value')]
+               )
+           ],
+           new \Magento\Framework\Search\Response\Aggregation([], [])
+       );
+
+       $this->logger->info('Search response: ' . json_encode($response));
+       
+       return $response;
+   }
 }
