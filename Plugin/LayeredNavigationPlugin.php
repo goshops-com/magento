901804944bfe93ea2a889668673470ddeabc7c5a
Santiago Cotto <scotto@goshops.ai>
2024-11-04 19:07:59 -0300

    asd

--- Plugin/LayeredNavigationPlugin.php
+++ Plugin/LayeredNavigationPlugin.php
@@ -2,39 +2,35 @@
 namespace Gopersonal\Magento\Plugin;
 
 use Magento\Catalog\Model\Layer\Filter\DataProvider\Price;
-use Magento\Framework\Registry;
 use Psr\Log\LoggerInterface;
+use Magento\Framework\App\RequestInterface;
 
 class LayeredNavigationPlugin
 {
-    protected $registry;
     protected $logger;
+    protected $request;
 
     public function __construct(
-        Registry $registry,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        RequestInterface $request
     ) {
-        $this->registry = $registry;
         $this->logger = $logger;
+        $this->request = $request;
     }
 
     public function beforeGetCollection(
         \Magento\Catalog\Model\Layer\Filter\AbstractFilter $subject
     ) {
         var_dump("LayeredNavigationPlugin - beforeGetCollection called");
-        
-        // Get the request object
-        $request = \Magento\Framework\App\ObjectManager::getInstance()
-            ->get(\Magento\Framework\App\RequestInterface::class);
             
-        if ($request->getParam('gpSearchOverride')) {
+        if ($this->request->getParam('gpSearchOverride')) {
             var_dump("Custom search is active in layered navigation");
             
             // Get the collection
             $collection = $subject->getLayer()->getProductCollection();
             
-            // Get product IDs from registry (we'll set this in SearchEngine)
-            $customProductIds = $this->registry->registry('custom_search_product_ids');
+            // Get product IDs from request
+            $customProductIds = $this->request->getParam('custom_product_ids');
             
             if ($customProductIds) {
                 var_dump("Found custom product IDs:", $customProductIds);
--- Plugin/SearchEngine.php
+++ Plugin/SearchEngine.php
@@ -2,23 +2,37 @@
 namespace Gopersonal\Magento\Plugin;
 
 use Magento\Framework\Search\RequestInterface;
-use Magento\Framework\Registry;
-// ... (keep existing use statements)
+use Magento\Framework\Api\Search\Document as SearchDocument;
+use Magento\Framework\Search\Response\QueryResponse;
+use Magento\Framework\Search\Response\Aggregation;
+use Magento\Framework\Search\Response\Aggregation\Value;
+use Psr\Log\LoggerInterface;
+use Magento\Framework\App\RequestInterface as HttpRequestInterface;
+use Magento\Search\Model\SearchEngine as MagentoSearchEngine;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Search\Model\AdapterFactory;
+use Magento\Framework\Search\Dynamic\IntervalFactory;
 
 class SearchEngine extends MagentoSearchEngine
 {
-    protected $registry;
-    
+    protected $logger;
+    protected $httpRequest;
+    protected $objectManager;
+    protected $adapterFactory;
+    protected $intervalFactory;
+
     public function __construct(
         AdapterFactory $adapterFactory,
         IntervalFactory $intervalFactory,
         LoggerInterface $logger,
         HttpRequestInterface $httpRequest,
-        ObjectManagerInterface $objectManager,
-        Registry $registry
+        ObjectManagerInterface $objectManager
     ) {
-        $this->registry = $registry;
-        // ... (keep existing constructor assignments)
+        $this->adapterFactory = $adapterFactory;
+        $this->intervalFactory = $intervalFactory;
+        $this->logger = $logger;
+        $this->httpRequest = $httpRequest;
+        $this->objectManager = $objectManager;
         parent::__construct($adapterFactory, $intervalFactory);
     }
 
@@ -32,7 +46,6 @@ class SearchEngine extends MagentoSearchEngine
         var_dump("USING CUSTOM SEARCH ENGINE");
         
         try {
-            // Your existing products array
             $products = [
                 [
                     'entity_id' => '1',
@@ -48,13 +61,71 @@ class SearchEngine extends MagentoSearchEngine
                 ]
             ];
 
-            // Store product IDs in registry for layered navigation
+            // Store the product IDs in the request
             $productIds = array_column($products, 'entity_id');
-            $this->registry->register('custom_search_product_ids', $productIds, true);
-            
-            var_dump("Stored product IDs in registry:", $productIds);
+            $this->httpRequest->setParam('custom_product_ids', $productIds);
+            var_dump("Stored product IDs in request:", $productIds);
 
-            // ... (rest of your existing search implementation)
+            $documents = [];
+            foreach ($products as $product) {
+                // Rest of your existing code...
+                $documentData = [
+                    'entity_id' => $product['entity_id'],
+                    'id' => $product['entity_id'],
+                    '_id' => $product['entity_id'],
+                    '_score' => 1,
+                    'score' => 1,
+                    'visibility' => 4,
+                    'status' => 1,
+                    'type_id' => 'simple',
+                    'store_id' => 1,
+                    'website_ids' => [1],
+                    '_type' => 'product',
+                    '_index' => 'catalog_product',
+                    '_source' => [
+                        'entity_id' => $product['entity_id'],
+                        'name' => $product['name'],
+                        'sku' => $product['sku'],
+                        'price' => $product['price'],
+                        'status' => 1,
+                        'visibility' => 4,
+                        'type_id' => 'simple',
+                        'store_id' => 1
+                    ]
+                ];
+
+                $documents[] = new SearchDocument(
+                    $documentData,
+                    [
+                        'entity_id' => new Value($product['entity_id'], 'entity_id'),
+                        'name' => new Value($product['name'], 'name'),
+                        'price' => new Value($product['price'], 'price'),
+                        'sku' => new Value($product['sku'], 'sku'),
+                        'status' => new Value(1, 'status'),
+                        'visibility' => new Value(4, 'visibility'),
+                        'store_id' => new Value(1, 'store_id'),
+                        'score' => new Value(1, 'score')
+                    ]
+                );
+            }
+
+            $aggregations = new Aggregation(
+                [
+                    'price_bucket' => new Value(99.99, 'price'),
+                ],
+                [
+                    'price_bucket' => [
+                        'name' => 'price_bucket',
+                        'values' => [
+                            ['from' => 0, 'to' => 100, 'count' => 1],
+                            ['from' => 100, 'to' => 200, 'count' => 1]
+                        ]
+                    ]
+                ]
+            );
+
+            return new QueryResponse($documents, $aggregations, count($documents));
+
         } catch (\Exception $e) {
             var_dump("Error in search engine:", $e->getMessage());
             throw $e;
