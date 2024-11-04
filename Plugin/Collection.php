b18ce62c90415edc3bd8de03a28f41d1fb58a2f7
Santiago Cotto <scotto@goshops.ai>
2024-11-04 10:33:23 -0300

    asd

--- Plugin/Collection.php
+++ Plugin/Collection.php
@@ -5,14 +5,6 @@ class Collection extends \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Col
 {
     public function getFoundIds()
     {
-        file_put_contents('/var/www/html/var/log/search_debug.log', 
-            "GetFoundIds called with query: " . $this->queryText . "\n", 
-            FILE_APPEND);
-
-        if ($this->queryText) {
-            return [2040];
-        }
-        
-        return parent::getFoundIds();
+        return [2040];
     }
 }
