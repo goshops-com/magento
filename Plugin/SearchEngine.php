ede0eb528fd2ccc7b2125f0eefa48a341d5e86d7
Santiago Cotto <scotto@goshops.ai>
2024-11-04 13:27:21 -0300

    asd

--- Plugin/SearchEngine.php
+++ Plugin/SearchEngine.php
@@ -1,50 +1,39 @@
-<?php
-namespace Gopersonal\Magento\Plugin;
-
-use Magento\Framework\Search\RequestInterface;
 use Magento\Framework\Api\Search\Document as SearchDocument;
-use Psr\Log\LoggerInterface;
-use Magento\Framework\Search\Response\QueryResponse;
+use Magento\Framework\Search\Response\AggregationFactory;
 
-class SearchEngine extends \Magento\Search\Model\SearchEngine
+public function __construct(
+    LoggerInterface $logger,
+    AggregationFactory $aggregationFactory
+) {
+    $this->logger = $logger;
+    $this->aggregationFactory = $aggregationFactory;
+}
+
+public function search(RequestInterface $request)
 {
-    protected $logger;
+    var_dump("SEARCH ENGINE CALLED2");
 
-    public function __construct(
-        LoggerInterface $logger
-    ) {
-        $this->logger = $logger;
-    }
+    $documentData = [
+        'entity_id' => '1',
+        'status' => 1,
+        'visibility' => 4,
+        'score' => 1
+    ];
 
-    public function search(RequestInterface $request)
-    {
-        var_dump("SEARCH ENGINE CALLED2");
-        
-        // Create document with entity_id as string
-        $documentData = [
-            'entity_id' => '2040',  // Changed to string
-            '_id' => 2040,
-            '_source' => [
-                'entity_id' => '2040',
-                'status' => 1,
-                'visibility' => 4,
-                'score' => 1
-            ]
-        ];
-        
-        var_dump("DOCUMENT DATA:", $documentData);
-        
-        $document = new SearchDocument(
-            $documentData,
-            ['score' => new \Magento\Framework\Search\Response\Aggregation\Value(1.0, 'value')]
-        );
-        
-        $response = new QueryResponse(
-            [$document],
-            new \Magento\Framework\Search\Response\Aggregation([], []),
-            1
-        );
-        
-        return $response;
-    }
+    var_dump("DOCUMENT DATA:", $documentData);
+
+    $document = new SearchDocument(
+        '2040', // ID as string
+        $documentData
+    );
+
+    $aggregations = $this->aggregationFactory->create([]);
+
+    $response = new QueryResponse(
+        [$document],
+        $aggregations,
+        1 // total count
+    );
+
+    return $response;
 }
