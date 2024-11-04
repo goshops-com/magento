a68764d7c344bf236e4dcd29d2d3bed0872d353b
Santiago Cotto <scotto@goshops.ai>
2024-11-04 14:55:26 -0300

    ads

--- Plugin/SearchEngine.php
+++ Plugin/SearchEngine.php
@@ -7,27 +7,43 @@ use Magento\Framework\Search\Response\QueryResponse;
 use Magento\Framework\Search\Response\Aggregation;
 use Magento\Framework\Search\Response\Aggregation\Value;
 use Psr\Log\LoggerInterface;
+use Magento\Framework\App\RequestInterface as HttpRequestInterface;
+use Magento\Elasticsearch\SearchAdapter\SearchIndexNameResolver;
+use Magento\Elasticsearch\Model\Client\ClientInterface;
 
 class SearchEngine extends \Magento\Search\Model\SearchEngine
 {
     protected $logger;
+    protected $httpRequest;
+    protected $searchIndexResolver;
+    protected $elasticsearchClient;
 
     public function __construct(
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        HttpRequestInterface $httpRequest,
+        SearchIndexNameResolver $searchIndexResolver,
+        ClientInterface $elasticsearchClient
     ) {
         $this->logger = $logger;
+        $this->httpRequest = $httpRequest;
+        $this->searchIndexResolver = $searchIndexResolver;
+        $this->elasticsearchClient = $elasticsearchClient;
     }
 
     public function search(RequestInterface $request)
     {
-        // var_dump("SEARCH ENGINE CALLED");
-        
-        // Debug request information safely
-        // var_dump("Request Dimensions:", $request->getDimensions());
-        // var_dump("Request Name:", $request->getName());
+        if (!$this->httpRequest->getParam('gpSearchOverride')) {
+            var_dump("USING DEFAULT MAGENTO SEARCH");
+            // Call Elasticsearch directly instead of using parent
+            $indexName = $this->searchIndexResolver->getIndexName();
+            $result = $this->elasticsearchClient->query($this->buildDefaultQuery($request));
+            return $this->convertElasticsearchResponse($result);
+        }
+
+        var_dump("USING CUSTOM SEARCH ENGINE");
         
         try {
-            // Create multiple products for testing
+            // Your custom search implementation
             $products = [
                 [
                     'entity_id' => '1',
@@ -71,8 +87,6 @@ class SearchEngine extends \Magento\Search\Model\SearchEngine
                     ]
                 ];
 
-                // var_dump("Creating document for product:", $product['entity_id']);
-
                 $documents[] = new SearchDocument(
                     $documentData,
                     [
@@ -88,7 +102,6 @@ class SearchEngine extends \Magento\Search\Model\SearchEngine
                 );
             }
 
-            // Create aggregations
             $aggregations = new Aggregation(
                 [
                     'price_bucket' => new Value(99.99, 'price'),
@@ -97,36 +110,63 @@ class SearchEngine extends \Magento\Search\Model\SearchEngine
                     'price_bucket' => [
                         'name' => 'price_bucket',
                         'values' => [
-                            [
-                                'from' => 0,
-                                'to' => 100,
-                                'count' => 1
-                            ],
-                            [
-                                'from' => 100,
-                                'to' => 200,
-                                'count' => 1
-                            ]
+                            ['from' => 0, 'to' => 100, 'count' => 1],
+                            ['from' => 100, 'to' => 200, 'count' => 1]
                         ]
                     ]
                 ]
             );
 
-            var_dump("Created documents count:", count($documents));
-
-            $response = new QueryResponse(
-                $documents,
-                $aggregations,
-                count($documents)
-            );
-
-            var_dump("RESPONSE CREATED WITH " . count($documents) . " DOCUMENTS");
-
-            return $response;
+            return new QueryResponse($documents, $aggregations, count($documents));
 
         } catch (\Exception $e) {
             var_dump("Error in search engine:", $e->getMessage());
             throw $e;
         }
+    }
+
+    private function buildDefaultQuery(RequestInterface $request)
+    {
+        // Build the elasticsearch query based on the request
+        // This is a basic example - you'll need to adapt this based on your needs
+        return [
+            'index' => $this->searchIndexResolver->getIndexName(),
+            'body' => [
+                'query' => [
+                    'bool' => [
+                        'must' => [
+                            ['match_all' => new \stdClass()]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    private function convertElasticsearchResponse($elasticsearchResponse)
+    {
+        // Convert Elasticsearch response to Magento's QueryResponse format
+        $documents = [];
+        if (isset($elasticsearchResponse['hits']['hits'])) {
+            foreach ($elasticsearchResponse['hits']['hits'] as $hit) {
+                $documentData = array_merge(
+                    ['_id' => $hit['_id'], '_score' => $hit['_score']],
+                    $hit['_source']
+                );
+                
+                $documents[] = new SearchDocument(
+                    $documentData,
+                    ['score' => new Value($hit['_score'], 'score')]
+                );
+            }
+        }
+
+        return new QueryResponse(
+            $documents,
+            new Aggregation([], []),
+            isset($elasticsearchResponse['hits']['total']['value']) 
+                ? $elasticsearchResponse['hits']['total']['value'] 
+                : 0
+        );
     }
 }
