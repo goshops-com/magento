c6430cca448666b4d0f2a191adc8ba65317d39da
Santiago Cotto <scotto@goshops.ai>
2024-11-04 11:19:01 -0300

    asd

--- Plugin/SearchEngine.php
+++ Plugin/SearchEngine.php
@@ -4,11 +4,10 @@ namespace Gopersonal\Magento\Plugin;
 use Magento\Framework\Search\ResponseInterface;
 use Magento\Framework\Search\RequestInterface;
 
-class SearchEngine implements \Magento\Search\Model\SearchEngine
+class SearchEngine extends \Magento\Search\Model\SearchEngine
 {
     public function search(RequestInterface $request)
     {
-        // Return hardcoded search result with product ID 2040
         return new \Magento\Framework\Search\Response\QueryResponse(
             [
                 new \Magento\Framework\Search\Document(
