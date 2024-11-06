4c7ab62a5915365ddcd98c4aaf000b5749acf60d
Santiago Cotto <scotto@goshops.ai>
2024-11-06 12:32:01 -0300

    asd

--- Plugin/SearchEnginePlugin.php
+++ Plugin/SearchEnginePlugin.php
@@ -10,10 +10,6 @@ use Psr\Log\LoggerInterface;
 use Magento\Framework\App\RequestInterface as HttpRequestInterface;
 use Magento\Search\Model\SearchEngine;
 use Magento\Framework\ObjectManagerInterface;
-use Magento\Catalog\Model\ResourceModel\Product\Attribute\CollectionFactory as AttributeCollectionFactory;
-use Magento\Catalog\Model\Layer\Category\FilterableAttributeList;
-use Magento\Store\Model\StoreManagerInterface;
-use Magento\Catalog\Model\Product\Attribute\Repository as AttributeRepository;
 
 class SearchEnginePlugin
 {
@@ -22,207 +18,15 @@ class SearchEnginePlugin
     protected $logger;
     protected $httpRequest;
     protected $objectManager;
-    protected $attributeCollectionFactory;
-    protected $filterableAttributeList;
-    protected $storeManager;
-    protected $attributeRepository;
 
     public function __construct(
         LoggerInterface $logger,
         HttpRequestInterface $httpRequest,
-        ObjectManagerInterface $objectManager,
-        AttributeCollectionFactory $attributeCollectionFactory,
-        FilterableAttributeList $filterableAttributeList,
-        StoreManagerInterface $storeManager,
-        AttributeRepository $attributeRepository
+        ObjectManagerInterface $objectManager
     ) {
         $this->logger = $logger;
         $this->httpRequest = $httpRequest;
         $this->objectManager = $objectManager;
-        $this->attributeCollectionFactory = $attributeCollectionFactory;
-        $this->filterableAttributeList = $filterableAttributeList;
-        $this->storeManager = $storeManager;
-        $this->attributeRepository = $attributeRepository;
-    }
-
-    /**
-     * Get all filterable attributes
-     *
-     * @return array
-     */
-    protected function getFilterableAttributes(): array
-    {
-        $attributes = $this->filterableAttributeList->getList();
-        $filterableAttributes = [];
-        
-        foreach ($attributes as $attribute) {
-            $filterableAttributes[$attribute->getAttributeCode()] = [
-                'code' => $attribute->getAttributeCode(),
-                'frontend_label' => $attribute->getFrontendLabel(),
-                'backend_type' => $attribute->getBackendType(),
-                'frontend_input' => $attribute->getFrontendInput(),
-                'options' => $this->getAttributeOptions($attribute)
-            ];
-        }
-        
-        $this->logger->debug('Filterable attributes: ' . print_r($filterableAttributes, true));
-        
-        return $filterableAttributes;
-    }
-
-    /**
-     * Get attribute options
-     *
-     * @param \Magento\Catalog\Model\ResourceModel\Eav\Attribute $attribute
-     * @return array
-     */
-    protected function getAttributeOptions($attribute): array
-    {
-        $options = [];
-        if ($attribute->usesSource()) {
-            foreach ($attribute->getSource()->getAllOptions() as $option) {
-                if (!empty($option['value'])) {
-                    $options[$option['value']] = [
-                        'value' => $option['value'],
-                        'label' => $option['label']
-                    ];
-                }
-            }
-        }
-        return $options;
-    }
-
-    /**
-     * Handle different attribute types for bucket values
-     *
-     * @param mixed $value
-     * @param string $attributeCode
-     * @param array $attribute
-     * @return mixed
-     */
-    protected function formatAttributeValue($value, string $attributeCode, array $attribute)
-    {
-        switch ($attributeCode) {
-            case 'price':
-                return number_format((float)$value, 2, '_', '');
-            
-            case 'category_ids':
-                return (string)$value;
-
-            default:
-                // For select/multiselect attributes, ensure we have string values
-                if (isset($attribute['frontend_input']) 
-                    && in_array($attribute['frontend_input'], ['select', 'multiselect'])
-                ) {
-                    return (string)$value;
-                }
-                return $value;
-        }
-    }
-
-    /**
-     * Create bucket based on attribute type
-     *
-     * @param string $attributeCode
-     * @param array $values
-     * @param array $attribute
-     * @return \Magento\Framework\Search\Response\Bucket
-     */
-    protected function createBucket(string $attributeCode, array $values, array $attribute)
-    {
-        $bucketName = $attributeCode . self::BUCKET_SUFFIX;
-        
-        switch ($attributeCode) {
-            case 'price':
-                $bucketValues = [];
-                foreach ($values as $value => $count) {
-                    $rangeLower = floor((float)$value / 10) * 10;
-                    $rangeUpper = $rangeLower + 10;
-                    $rangeKey = $rangeLower . '_' . $rangeUpper;
-                    
-                    if (!isset($bucketValues[$rangeKey])) {
-                        $bucketValues[$rangeKey] = [
-                            'value' => $rangeKey,
-                            'count' => 0,
-                            'from' => $rangeLower,
-                            'to' => $rangeUpper
-                        ];
-                    }
-                    $bucketValues[$rangeKey]['count'] += $count;
-                }
-                
-                return new \Magento\Framework\Search\Response\Bucket(
-                    $bucketName,
-                    array_map(function($range) use ($bucketName) {
-                        return new Value($range['value'], $range, $bucketName);
-                    }, array_values($bucketValues))
-                );
-
-            case 'category_ids':
-                return new \Magento\Framework\Search\Response\Bucket(
-                    $bucketName,
-                    array_map(function($value, $count) use ($bucketName) {
-                        return new Value((string)$value, [
-                            'value' => $value,
-                            'count' => $count
-                        ], $bucketName);
-                    }, array_keys($values), array_values($values))
-                );
-                
-            default:
-                return new \Magento\Framework\Search\Response\Bucket(
-                    $bucketName,
-                    array_map(function($value, $count) use ($bucketName, $attribute) {
-                        $metrics = [
-                            'value' => $value,
-                            'count' => $count
-                        ];
-                        
-                        // Add label for select/multiselect attributes
-                        if (isset($attribute['frontend_input']) 
-                            && in_array($attribute['frontend_input'], ['select', 'multiselect'])
-                            && isset($attribute['options'][$value]['label'])
-                        ) {
-                            $metrics['label'] = $attribute['options'][$value]['label'];
-                        }
-                        
-                        return new Value((string)$value, $metrics, $bucketName);
-                    }, array_keys($values), array_values($values))
-                );
-        }
-    }
-
-    /**
-     * Get attribute value counts from products with type handling
-     *
-     * @param array $products
-     * @param string $attributeCode
-     * @param array $attribute
-     * @return array
-     */
-    protected function getAttributeCounts(array $products, string $attributeCode, array $attribute): array 
-    {
-        $counts = [];
-        foreach ($products as $product) {
-            if (isset($product[$attributeCode])) {
-                if (is_array($product[$attributeCode])) {
-                    // Handle multiselect attributes (like category_ids)
-                    foreach ($product[$attributeCode] as $value) {
-                        if (!isset($counts[$value])) {
-                            $counts[$value] = 0;
-                        }
-                        $counts[$value]++;
-                    }
-                } else {
-                    $value = $product[$attributeCode];
-                    if (!isset($counts[$value])) {
-                        $counts[$value] = 0;
-                    }
-                    $counts[$value]++;
-                }
-            }
-        }
-        return $counts;
     }
 
     public function aroundSearch(
@@ -233,29 +37,19 @@ class SearchEnginePlugin
         $this->logger->debug("SearchEnginePlugin aroundSearch() called");
         
         if (!$this->httpRequest->getParam('gpSearchOverride')) {
-            $result = $proceed($request);
-            if ($result instanceof QueryResponse) {
-                // Log the default response structure
-                $buckets = $result->getAggregations()->getBuckets();
-                foreach ($buckets as $key => $bucket) {
-                    $this->logger->debug("Default bucket {$key}: " . print_r([
-                        'name' => $bucket->getName(),
-                        'values' => array_map(function($value) {
-                            return [
-                                'value' => $value->getValue(),
-                                'metrics' => $value->getMetrics(),
-                                'field' => $value->getField()
-                            ];
-                        }, $bucket->getValues())
-                    ], true));
-                }
-            }
-            return $result;
+            return $proceed($request);
         }
 
         $this->logger->debug("SearchEnginePlugin: USING CUSTOM SEARCH ENGINE");
         
         try {
+            // Get default response first to preserve bucket structure
+            $defaultResponse = $proceed($request);
+            $this->logger->debug('Default buckets: ' . print_r(
+                array_keys($defaultResponse->getAggregations()->getBuckets()),
+                true
+            ));
+
             // Products with multiple categories
             $products = [
                 [
@@ -292,8 +86,7 @@ class SearchEnginePlugin
                 $documents[] = $document;
             }
 
-            // Create category values
-            $categoryValues = [];
+            // Count products in each category
             $categoryCounts = [];
             foreach ($products as $product) {
                 foreach ($product['category_ids'] as $catId) {
@@ -304,52 +97,43 @@ class SearchEnginePlugin
                 }
             }
 
+            // Create buckets array
+            $buckets = [];
+
+            // Price bucket
+            $buckets['price_bucket'] = new \Magento\Framework\Search\Response\Bucket(
+                'price_bucket',
+                [
+                    new Value('90_100', [
+                        'from' => 90,
+                        'to' => 100,
+                        'count' => 1,
+                        'value' => '90_100'
+                    ], 'price_bucket'),
+                    new Value('140_150', [
+                        'from' => 140,
+                        'to' => 150,
+                        'count' => 1,
+                        'value' => '140_150'
+                    ], 'price_bucket')
+                ]
+            );
+
+            // Category bucket - note we use category_bucket consistently
+            $categoryValues = [];
             foreach ($categoryCounts as $catId => $count) {
                 $categoryValues[] = new Value((string)$catId, [
                     'value' => $catId,
-                    'count' => $count,
-                    'title' => "Category {$catId}"  // Added title
-                ], 'category');
+                    'count' => $count
+                ], 'category_bucket');
             }
+            
+            $buckets['category_bucket'] = new \Magento\Framework\Search\Response\Bucket(
+                'category_bucket',
+                $categoryValues
+            );
 
-            // Create buckets
-            $buckets = [
-                'price_bucket' => new \Magento\Framework\Search\Response\Bucket(
-                    'price_bucket',
-                    [
-                        new Value('90_100', [
-                            'from' => 90,
-                            'to' => 100,
-                            'count' => 1,
-                            'value' => '90_100'
-                        ], 'price_bucket'),
-                        new Value('140_150', [
-                            'from' => 140,
-                            'to' => 150,
-                            'count' => 1,
-                            'value' => '140_150'
-                        ], 'price_bucket')
-                    ]
-                ),
-                'category' => new \Magento\Framework\Search\Response\Bucket(
-                    'category',
-                    $categoryValues
-                )
-            ];
-
-            // Log the bucket structure we're creating
-            foreach ($buckets as $key => $bucket) {
-                $this->logger->debug("Custom bucket {$key}: " . print_r([
-                    'name' => $bucket->getName(),
-                    'values' => array_map(function($value) {
-                        return [
-                            'value' => $value->getValue(),
-                            'metrics' => $value->getMetrics(),
-                            'field' => $value->getField()
-                        ];
-                    }, $bucket->getValues())
-                ], true));
-            }
+            $this->logger->debug('Created buckets with names: ' . print_r(array_keys($buckets), true));
 
             $aggregations = new Aggregation($buckets);
             $response = new QueryResponse($documents, $aggregations, count($documents));
