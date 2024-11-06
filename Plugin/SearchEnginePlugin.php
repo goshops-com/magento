0b4e1eff24936217708a354298c1e4e24b0f917e
Santiago Cotto <scotto@goshops.ai>
2024-11-06 12:05:34 -0300

    asd

--- Plugin/SearchEnginePlugin.php
+++ Plugin/SearchEnginePlugin.php
@@ -10,6 +10,10 @@ use Psr\Log\LoggerInterface;
 use Magento\Framework\App\RequestInterface as HttpRequestInterface;
 use Magento\Search\Model\SearchEngine;
 use Magento\Framework\ObjectManagerInterface;
+use Magento\Catalog\Model\ResourceModel\Product\Attribute\CollectionFactory as AttributeCollectionFactory;
+use Magento\Catalog\Model\Layer\Category\FilterableAttributeList;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Model\Product\Attribute\Repository as AttributeRepository;
 
 class SearchEnginePlugin
 {
@@ -18,15 +22,72 @@ class SearchEnginePlugin
     protected $logger;
     protected $httpRequest;
     protected $objectManager;
+    protected $attributeCollectionFactory;
+    protected $filterableAttributeList;
+    protected $storeManager;
+    protected $attributeRepository;
 
     public function __construct(
         LoggerInterface $logger,
         HttpRequestInterface $httpRequest,
-        ObjectManagerInterface $objectManager
+        ObjectManagerInterface $objectManager,
+        AttributeCollectionFactory $attributeCollectionFactory,
+        FilterableAttributeList $filterableAttributeList,
+        StoreManagerInterface $storeManager,
+        AttributeRepository $attributeRepository
     ) {
         $this->logger = $logger;
         $this->httpRequest = $httpRequest;
         $this->objectManager = $objectManager;
+        $this->attributeCollectionFactory = $attributeCollectionFactory;
+        $this->filterableAttributeList = $filterableAttributeList;
+        $this->storeManager = $storeManager;
+        $this->attributeRepository = $attributeRepository;
+    }
+
+    /**
+     * Get all filterable attributes
+     *
+     * @return array
+     */
+    protected function getFilterableAttributes(): array
+    {
+        $attributes = $this->filterableAttributeList->getList();
+        $filterableAttributes = [];
+        
+        foreach ($attributes as $attribute) {
+            $filterableAttributes[$attribute->getAttributeCode()] = [
+                'code' => $attribute->getAttributeCode(),
+                'frontend_label' => $attribute->getFrontendLabel(),
+                'options' => $this->getAttributeOptions($attribute)
+            ];
+        }
+        
+        $this->logger->debug('Filterable attributes: ' . print_r($filterableAttributes, true));
+        
+        return $filterableAttributes;
+    }
+
+    /**
+     * Get attribute options
+     *
+     * @param \Magento\Catalog\Model\ResourceModel\Eav\Attribute $attribute
+     * @return array
+     */
+    protected function getAttributeOptions($attribute): array
+    {
+        $options = [];
+        if ($attribute->usesSource()) {
+            foreach ($attribute->getSource()->getAllOptions() as $option) {
+                if (!empty($option['value'])) {
+                    $options[$option['value']] = [
+                        'value' => $option['value'],
+                        'label' => $option['label']
+                    ];
+                }
+            }
+        }
+        return $options;
     }
 
     public function aroundSearch(
@@ -57,23 +118,32 @@ class SearchEnginePlugin
                     'name' => 'Test Product 1',
                     'price' => 99.99,
                     'sku' => 'TEST-1',
-                    'category_ids' => [3, 9, 20]
+                    'category_ids' => [3, 9, 20],
+                    // Add sample values for other filterable attributes
+                    'color' => 'red',
+                    'size' => 'L',
+                    'manufacturer' => 'brand1'
                 ],
                 [
                     'entity_id' => '2',
                     'name' => 'Test Product 2',
                     'price' => 149.99,
                     'sku' => 'TEST-2',
-                    'category_ids' => [3, 11, 37]
+                    'category_ids' => [3, 11, 37],
+                    // Add sample values for other filterable attributes
+                    'color' => 'blue',
+                    'size' => 'M',
+                    'manufacturer' => 'brand2'
                 ]
             ];
 
-            // Create documents
+            // Get filterable attributes
+            $filterableAttributes = $this->getFilterableAttributes();
+
+            // Create documents with all filterable attributes
             $documents = [];
             foreach ($products as $product) {
-                $document = new Document();
-                $document->setId($product['entity_id']);
-                $document->setCustomAttributes([
+                $attributes = [
                     'entity_id' => new Value($product['entity_id'], 'entity_id'),
                     'name' => new Value($product['name'], 'name'),
                     'price' => new Value($product['price'], 'price'),
@@ -82,19 +152,19 @@ class SearchEnginePlugin
                     'visibility' => new Value(4, 'visibility'),
                     'store_id' => new Value(1, 'store_id'),
                     'category_ids' => new Value(implode(',', $product['category_ids']), 'category_ids')
-                ]);
-                $documents[] = $document;
-            }
+                ];
 
-            // Count products in each category
-            $categoryCounts = [];
-            foreach ($products as $product) {
-                foreach ($product['category_ids'] as $catId) {
-                    if (!isset($categoryCounts[$catId])) {
-                        $categoryCounts[$catId] = 0;
+                // Add all filterable attributes to document
+                foreach ($filterableAttributes as $code => $attribute) {
+                    if (isset($product[$code])) {
+                        $attributes[$code] = new Value($product[$code], $code);
                     }
-                    $categoryCounts[$catId]++;
                 }
+
+                $document = new Document();
+                $document->setId($product['entity_id']);
+                $document->setCustomAttributes($attributes);
+                $documents[] = $document;
             }
 
             // Create buckets array
@@ -119,19 +189,38 @@ class SearchEnginePlugin
                 ]
             );
 
-            // Category bucket - note we use category_bucket consistently
-            $categoryValues = [];
-            foreach ($categoryCounts as $catId => $count) {
-                $categoryValues[] = new Value((string)$catId, [
-                    'value' => $catId,
-                    'count' => $count
-                ], 'category_bucket');
-            }
-            
+            // Category bucket
+            $categoryValues = $this->getCategoryCounts($products);
             $buckets['category_bucket'] = new \Magento\Framework\Search\Response\Bucket(
                 'category_bucket',
-                $categoryValues
+                array_map(function($catId, $count) {
+                    return new Value((string)$catId, [
+                        'value' => $catId,
+                        'count' => $count
+                    ], 'category_bucket');
+                }, array_keys($categoryValues), array_values($categoryValues))
             );
+
+            // Add buckets for each filterable attribute
+            foreach ($filterableAttributes as $code => $attribute) {
+                $bucketName = $code . self::BUCKET_SUFFIX;
+                $attributeValues = $this->getAttributeCounts($products, $code);
+                
+                if (!empty($attributeValues)) {
+                    $bucketValues = [];
+                    foreach ($attributeValues as $value => $count) {
+                        $bucketValues[] = new Value((string)$value, [
+                            'value' => $value,
+                            'count' => $count
+                        ], $bucketName);
+                    }
+                    
+                    $buckets[$bucketName] = new \Magento\Framework\Search\Response\Bucket(
+                        $bucketName,
+                        $bucketValues
+                    );
+                }
+            }
 
             $this->logger->debug('Created buckets with names: ' . print_r(array_keys($buckets), true));
 
@@ -144,5 +233,47 @@ class SearchEnginePlugin
             $this->logger->error("SearchEnginePlugin Error: " . $e->getMessage());
             throw $e;
         }
+    }
+
+    /**
+     * Get category counts from products
+     *
+     * @param array $products
+     * @return array
+     */
+    protected function getCategoryCounts(array $products): array
+    {
+        $counts = [];
+        foreach ($products as $product) {
+            foreach ($product['category_ids'] as $catId) {
+                if (!isset($counts[$catId])) {
+                    $counts[$catId] = 0;
+                }
+                $counts[$catId]++;
+            }
+        }
+        return $counts;
+    }
+
+    /**
+     * Get attribute value counts from products
+     *
+     * @param array $products
+     * @param string $attributeCode
+     * @return array
+     */
+    protected function getAttributeCounts(array $products, string $attributeCode): array
+    {
+        $counts = [];
+        foreach ($products as $product) {
+            if (isset($product[$attributeCode])) {
+                $value = $product[$attributeCode];
+                if (!isset($counts[$value])) {
+                    $counts[$value] = 0;
+                }
+                $counts[$value]++;
+            }
+        }
+        return $counts;
     }
 }
