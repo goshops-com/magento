929b4cf93d3e3a68336e6017bed5c05279b9f191
Santiago Cotto <scotto@goshops.ai>
2024-11-06 22:22:01 -0300

    asd

--- Plugin/SearchEnginePlugin.php
+++ Plugin/SearchEnginePlugin.php
@@ -94,7 +94,7 @@ class SearchEnginePlugin
                     'name' => 'Test Product 1',
                     'price' => 99.99,
                     'sku' => 'TEST-1',
-                    'category_ids' => [3, 9, 20],
+                    'category_ids' => [3],
                     'size' => '166',      // XS
                 ],
                 [
@@ -102,9 +102,7 @@ class SearchEnginePlugin
                     'name' => 'Test Product 2',
                     'price' => 149.99,
                     'sku' => 'TEST-2',
-                    'category_ids' => [3, 11, 37],
-
-                    'size' => '167',      // S
+                    'category_ids' => [3]
                 ]
             ];
 
