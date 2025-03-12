d959c2c4cbded2c1e63635d9921b0dd8ba1c47e7
Santiago Cotto <scotto@goshops.ai>
2025-03-12 09:51:11 -0300

    asd

--- Plugin/SearchEnginePlugin.php
+++ Plugin/SearchEnginePlugin.php
@@ -253,6 +253,19 @@ class SearchEnginePlugin
         return $jsonFilter;
     }
 
+    public function afterGetAggregations(QueryResponse $subject, $result)
+    {
+        $bucketNames = [];
+        foreach ($result->getBuckets() as $bucket) {
+            $bucketNames[] = $bucket->getName();
+        }
+        $this->logger->debug(
+            'Available bucket names in search response: ' .
+                json_encode($bucketNames)
+        );
+        return $result;
+    }
+
     protected function getStoredBuckets(?string $gsSearchId): ?array
     {
         if (!$gsSearchId) {
