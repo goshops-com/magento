638760ce0b4de173cabb688189c2e22a79a1630c
Santiago Cotto <scotto@goshops.ai>
2026-05-13 14:17:57 -0300

    fix gp-firebase path for composer installs

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,27 +4,23 @@ namespace Gopersonal\Magento\Controller\Index;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\RawFactory;
-use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\View\Result\PageFactory;
 use Psr\Log\LoggerInterface; // Add the LoggerInterface
 
 class Index extends Action
 {
     protected $resultRawFactory;
-    protected $directoryList;
     protected $pageFactory;
     protected $logger; // Add the logger property
 
     public function __construct(
         Context $context,
         RawFactory $resultRawFactory,
-        DirectoryList $directoryList,
         PageFactory $pageFactory,
         LoggerInterface $logger // Inject the logger
     ) {
         parent::__construct($context);
         $this->resultRawFactory = $resultRawFactory;
-        $this->directoryList = $directoryList;
         $this->pageFactory = $pageFactory;
         $this->logger = $logger; // Assign the logger
     }
@@ -38,8 +34,7 @@ class Index extends Action
         if ($frontName === 'gp-firebase') {
             $resultRaw = $this->resultRawFactory->create();
 
-            $moduleDir = $this->directoryList->getPath('app');
-            $filePath = $moduleDir . '/code/Gopersonal/Magento/web/gp-firebase.js';
+            $filePath = dirname(__DIR__, 2) . '/web/gp-firebase.js';
             
             $jsContent = file_get_contents($filePath);
             
